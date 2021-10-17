8840011eb7debcab56d4633518c6c7dc904e8823
Sepand KASHANI <kashani.sepand@gmail.com>
2021-10-17 18:33:43 +0200

    Refactor shape-deduction helpers.
    * Rename broadcasted_sum() to broadcast_sum_shapes() to match NumPy's naming convention.
    * Rename broadcasted_matmul() to broadcast_matmul_shapes() to match NumPy's naming convention.
    * Speed-up broadcast_*_shapes() execution.

--- pycsou/util/__init__.py
+++ pycsou/util/__init__.py
@@ -2,29 +2,37 @@ import numpy as np
 import typing as typ
 
 
-def broadcasted_sum(shape1: typ.Tuple[int, ...], shape2: typ.Tuple[int, ...]) -> typ.Tuple[
-    bool, typ.Union[typ.Tuple[int, ...], None]]:
-    valid_shapes = True
-    try:
-        arr1 = np.zeros(shape1)
-        arr2 = np.zeros(shape2)
-        arr3 = arr1 + arr2
-        output_shape = arr3.shape
-    except ValueError:
-        valid_shapes = False
-        output_shape = None
-    return valid_shapes, output_shape
+def broadcast_sum_shapes(
+    shape1: typ.Tuple[int, ...],
+    shape2: typ.Tuple[int, ...],
+) -> typ.Tuple[int, ...]:
+    return np.broadcast_shapes(shape1, shape2)
 
 
-def broadcasted_matmul(shape1: typ.Tuple[int, ...], shape2: typ.Tuple[int, ...]) -> typ.Tuple[
-    bool, typ.Union[typ.Tuple[int, ...], None]]:
-    valid_shapes = True
-    try:
-        arr1 = np.zeros(shape1)
-        arr2 = np.zeros(shape2)
-        arr3 = arr1 @ arr2
-        output_shape = arr3.shape
-    except ValueError:
-        valid_shapes = False
-        output_shape = None
-    return valid_shapes, output_shape
+def broadcast_matmul_shapes(
+    shape1: typ.Tuple[int, ...],
+    shape2: typ.Tuple[int, ...],
+) -> typ.Tuple[int, ...]:
+    sh1_was_1d = False
+    if len(shape1) == 1:
+        sh1_was_1d = True
+        shape1 = (1,) + shape1
+
+    sh2_was_1d = False
+    if len(shape2) == 1:
+        sh2_was_1d = True
+        shape2 = shape2 + (1,)
+
+    if shape1[-1] != shape2[-2]:
+        raise ValueError(f"Cannot @-multiply shapes {shape1}, {shape2}.")
+
+    sh = np.broadcast_shapes(shape1[:-2], shape2[:-2])
+    if sh1_was_1d and sh2_was_1d:
+        pass
+    elif (not sh1_was_1d) and sh2_was_1d:
+        sh = sh + (shape1[-2],)
+    elif sh1_was_1d and (not sh2_was_1d):
+        sh = sh + (shape2[-1],)
+    else:
+        sh = sh + (shape1[-2], shape2[-1])
+    return sh
