c209f6c45b1f24f1d2fb0a630e476d023cccbd8c
MatthieuMeo <matthieu.simeoni@gmail.com>
2021-10-30 00:45:20 +0200

    Arithmetic (addition, composition and argshift/scale) fully implemented for classes [Map, DiffMap, Func, DiffFunc, ProxFunc, LinOp, LinFunc]. No tests yet. Drop support for complex maps.

--- src/pycsou/abc/operator.py
+++ src/pycsou/abc/operator.py
@@ -12,7 +12,7 @@ NDArray = pycsou.abc.NDArray
 
 class Property:
     @classmethod
-    def _property_list(cls):
+    def _property_list(cls) -> frozenset:
         return frozenset(
             ("apply", "_lipschitz", "jacobian", "_diff_lipschitz", "single_valued", "gradient", "prox", "adjoint")
         )
@@ -52,8 +52,8 @@ class Property:
                 setattr(out_op, prop, types.MethodType(composite_method, out_op))
         return out_op.squeeze()
 
-    def __mul__(self: "Map", other: typ.Union["Map", nb.Number]) -> "Map":
-        if isinstance(other, nb.Number):
+    def __mul__(self: "Map", other: typ.Union["Map", nb.Real]) -> "Map":
+        if isinstance(other, nb.Real):
             hmap = _HomothetyOp(other, dim=self.shape[0])
             return hmap.__mul__(self)
         elif not isinstance(other, Map):
@@ -64,6 +64,8 @@ class Property:
             raise ValueError(f"Cannot compose two maps with inconsistent shapes {self.shape} and {other.shape}.")
         shared_props = self.properties() & other.properties()
         shared_props.discard("prox")
+        if self.shape[0] == 1 and "jacobian" in shared_props:
+            shared_props.update({"gradient", "single_valued"})
         for Op in _base_operators:
             if Op.properties() == shared_props:
                 break
@@ -71,31 +73,95 @@ class Property:
             shared_props.discard("jacobian")
         shared_props.discard("single_valued")
         out_op = Op(out_shape)
-        for prop in shared_props:
-            if prop in ["_lispchitz", "_diff_lipschitz"]:
-                setattr(out_op, prop, getattr(self, prop) + getattr(other, prop))
-            else:
+        for prop in shared_props:  # ("apply", "_lipschitz", "jacobian", "_diff_lipschitz", "gradient", "adjoint")
+            if prop == "apply":
+                out_op.apply = types.MethodType(lambda obj, arr: self.apply(other.apply(arr)), out_op)
+            elif prop == "_lipschitz":
+                out_op._lipschitz = self._lipschitz * other._lipschitz
+            elif prop == "_diff_lipschitz":
+                if isinstance(self, LinOp):
+                    out_op._diff_lipschitz = self._lipschitz * other._diff_lipschitz
+                elif isinstance(other, LinOp):
+                    out_op._diff_lipschitz = self._diff_lipschitz * (other._lipschitz) ** 2
+                else:
+                    out_op._diff_lipschitz = np.infty
+            elif prop == "gradient":
 
-                def composite_method(obj, arr: NDArray) -> typ.Union[NDArray, "LinOp"]:
-                    return getattr(self, prop)(arr) + getattr(other, prop)(arr)
+                def composite_gradient(obj, arr: NDArray) -> NDArray:
+                    return other.jacobian(arr).adjoint(self.gradient(other.apply(arr)))
 
-                setattr(out_op, prop, types.MethodType(composite_method, out_op))
+                out_op.gradient = types.MethodType(composite_gradient, out_op)
+            elif prop == "jacobian":
+
+                def composite_jacobian(obj, arr: NDArray) -> "LinOp":
+                    return self.jacobian(other.apply(arr)) * other.jacobian(arr)
+
+                out_op.jacobian = types.MethodType(composite_jacobian, out_op)
+            elif prop == "adjoint":
+                out_op.adjoint = types.MethodType(lambda obj, arr: other.adjoint(self.adjoint(arr)), out_op)
         return out_op.squeeze()
 
-    def __rmul__(self, other: nb.Number) -> "Map":
+    def __rmul__(self: "Map", other: nb.Real) -> "Map":
         return self.__mul__(other)
 
-    def __pow__(self, power: float) -> "Map":
-        pass
+    def __pow__(self: "Map", power: float) -> "Map":
+        if type(power) is int:
+            if power == 0:
+                from pycsou.linop.base import IdentityOperator
 
-    def __neg__(self) -> "Map":
-        pass
+                exp_map = IdentityOperator(shape=self.shape)
+            else:
+                exp_map = self
+                for i in range(1, power):
+                    exp_map = self.__mul__(exp_map)
+            return exp_map
+        else:
+            raise NotImplementedError
+
+    def __neg__(self: "Map") -> "Map":
+        return self.__mul__(-1)
+
+    def __sub__(self: "Map", other: "Map") -> "Map":
+        return self.__add__(other.__neg__())
+
+    def __truediv__(self: "Map", scalar: nb.Real) -> "Map":
+        if isinstance(scalar, nb.Real):
+            return self.__mul__(1 / scalar)
+        else:
+            raise NotImplementedError
 
     def argscale(self, scalar: typ.Union[float, "UnitOp"]) -> "Map":
-        pass
+        if isinstance(other, nb.Real):
+            hmap = _HomothetyOp(other, dim=self.shape[1])
+            return self.__mul__(hmap)
+        else:
+            raise NotImplementedError
 
     def argshift(self, arr: NDArray) -> "Map":
-        pass
+        try:
+            arr = arr.copy().squeeze()
+        except:
+            raise ValueError("Argument [arr] must be of type NDArray.")
+        if arr.shape[-1] != self.shape[-1]:
+            raise ValueError(f"Invalid lag shape: {arr.shape[-1]} != {self.shape[-1]}")
+        out_op = self.__class__(shape=self.shape)
+        props = out_op.properties()
+        if Op in [LinOp, DiffFunc, LinFunc]:
+            props.discard("jacobian")
+        props.discard("single_valued")
+        props.discard("prox")
+        for (
+            prop
+        ) in out_op.properties():  # ("apply", "_lipschitz", "jacobian", "_diff_lipschitz", "gradient", "adjoint")
+            if prop in ["_lispchitz", "_diff_lipschitz"]:
+                setattr(out_op, prop, getattr(self, prop))
+            else:
+
+                def argshifted_method(obj, x: NDArray) -> typ.Union[NDArray, "LinOp"]:
+                    return getattr(self, prop)(x + arr)
+
+                setattr(out_op, prop, types.MethodType(argshifted_method, out_op))
+        return out_op.squeeze()
 
 
 class SingledValued(Property):
@@ -224,16 +290,28 @@ class ProxFunc(Func, Proximal):
         super(ProxFunc, self).__init__(shape)
 
     def __add__(self: "ProxFunc", other: "Map") -> "Map":
+        f = Property.__add__(self, other)
         if isinstance(other, LinFunc):
-            valid_shapes, out_shape = pycutil.broadcast_sum_shapes(self.shape, other.shape)
-            if not valid_shapes:
-                raise ValueError(f"Cannot sum two maps with inconsistent shapes {self.shape} and {other.shape}.")
-            f = ProxFunc(out_shape)
-            f.apply = types.MethodType(lambda _, x: self.apply(x) + other.apply(x), f)
-            f._lipschitz = self._lipschitz + other._lipschitz
+            f = f.specialize(cast_to=ProxFunc)
             f.prox = types.MethodType(lambda _, x, tau: self.prox(x - tau * other.asarray(), tau), f)
-        else:
-            f = Property.__add__(self, other)
+        return f.squeeze()
+
+    def __mul__(self: ProxFunc, other: Map) -> typ.Union[ProxFunc, Map]:
+        f = Property.__mul__(self, other)
+        if isinstance(other, UnitOp):
+            f.specialize(cast_to=ProxFunc)
+            f.prox = types.MethodType(lambda obj, arr, tau: other.adjoint(self.prox(other.apply(arr), tau)), f)
+        elif isinstance(other, _HomothetyOp):
+            f.specialize(cast_to=ProxFunc)
+            f.prox = types.MethodType(
+                lambda obj, arr, tau: (1 / other._cst) * self.prox(other._cst * arr, tau * (other._cst) ** 2), f
+            )
+        return f.squeeze()
+
+    def argshift(self, arr: NDArray) -> "Map":
+        f = Property.argshift(self, arr)
+        f = f.specialize(cast_to=ProxFunc)
+        f.prox = types.MethodType(lambda obj, x, tau: self.prox(x + arr, tau) - arr, f)
         return f
 
 
@@ -358,9 +436,9 @@ class PosDefOp(SelfAdjointOp):
 
 
 class _HomothetyOp(SelfAdjointOp):
-    def __init__(self, cst: nb.Number, dim: int):
-        if not isinstance(cst, nb.Number):
-            raise ValueError("Argument [cst] must be a number.")
+    def __init__(self, cst: nb.Real, dim: int):
+        if not isinstance(cst, nb.Real):
+            raise ValueError("Argument [cst] must be a real number.")
         super(_HomothetyOp, self).__init__(shape=(dim, dim))
         self._cst = cst
         self._lipschitz = np.abs(cst)
@@ -371,6 +449,13 @@ class _HomothetyOp(SelfAdjointOp):
 
     def adjoint(self, arr: NDArray) -> NDArray:
         return (self._cst * arr).astype(arr.dtype)
+
+    def __mul__(self, other):
+        out_op = Property.__mul__(self, other)
+        if isinstance(other, ProxFunc):
+            out_op.specialize(cast_to=ProxFunc)
+            out_op.prox = types.MethodType(lambda obj, arr, tau: other.prox(arr, self._cst * tau), out_op)
+        return outop
 
 
 _base_operators = frozenset([Map, DiffMap, Func, DiffFunc, ProxFunc, LinOp, LinFunc])
