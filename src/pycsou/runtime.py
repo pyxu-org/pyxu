ca9f3f625fca1b6bc0e9ed8712b17294c2286b8e
Sepand KASHANI <kashani.sepand@gmail.com>
2021-11-01 23:05:21 +0100

    runtime: enable precision control via decorator.

--- src/pycsou/runtime.py
+++ src/pycsou/runtime.py
@@ -1,5 +1,8 @@
+import collections.abc as cabc
 import contextlib
 import enum
+import functools
+import inspect
 
 import numpy as np
 
@@ -46,6 +49,61 @@ class Precision(contextlib.AbstractContextManager):
 
         _setPrecision(self._width_prev)
         return False if exc_raised else True
+
+
+def enforce_precision(i: cabc.Collection[str] = frozenset(), o: bool = True) -> cabc.Callable:
+    """
+    Decorator to pre/post-process function parameters to enforce runtime FP-precision.
+
+    Parameters
+    ----------
+    i: cabc.Collection[str]
+        Function parameters for which precision must be enforced to runtime's FP-precision.
+        Function parameter values must have a NumPy API.
+    o: bool
+        If True (default), ensure function's output has runtime's FP-precision.
+        If function's output does not have a NumPy API, set `o` explicitly to False.
+
+    Example
+    -------
+    >>> import pycsou.runtime as pyrt
+    >>> @pyrt.enforce_precision(i='y', o=False)  # `i` can process multiple args: `i=('x','y')`.
+    ... def f(x, y, z=1):
+    ...     print(x.dtype, y.dtype)
+    ...     return x + y + z
+    ...
+    ... x = np.arange(5)
+    ... y = np.r_[0.5]
+    ... print(x.dtype, y.dtype)                  # int64, float64
+    ... with pyrt.Precision(pyrt.Width.SINGLE):
+    ...     out = f(x,y)                         # int64, float32 (printed inside f-call.)
+    ... print(out.dtype)                         # float64 (would have been float32 if `o=True`)
+    """
+
+    def decorator(func: cabc.Callable) -> cabc.Callable:
+        @functools.wraps(func)
+        def wrapper(*ARGS, **KWARGS):
+            dtype = getPrecision().value
+
+            sig = inspect.Signature.from_callable(func)
+            func_args = sig.bind(*ARGS, **KWARGS)
+            func_args.apply_defaults()
+            func_args = func_args.arguments
+            for k in i:
+                if k not in func_args:
+                    error_msg = f"Parameter[{k}] not part of {func.__qualname__}() parameter list."
+                    raise ValueError(error_msg)
+                else:  # change input precision
+                    func_args[k] = func_args[k].astype(dtype, copy=False)
+
+            out = func(**func_args)
+            if o and (out is not None):
+                out = out.astype(dtype, copy=False)
+            return out
+
+        return wrapper
+
+    return decorator
 
 
 def getPrecision() -> Width:
