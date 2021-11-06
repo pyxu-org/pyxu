ae4f44bac51857c0fa47e5aa160ecd32e1f9df74
Sepand KASHANI <kashani.sepand@gmail.com>
2021-11-06 12:53:00 +0100

    [test][shape_inference] Use parameterized tests. (pytest best-practices)

--- tests/util/test_infer_shape.py
+++ tests/util/test_infer_shape.py
@@ -3,48 +3,60 @@ import pytest
 import pycsou.util as pycu
 
 
-def test_infer_sum_shape():
-    valid = (
-        ((5, 3), (5, 3), (5, 3)),  # same shape
-        ((5, 3), (1, 3), (5, 3)),  # codomain broadcast
-        ((1, 3), (5, 3), (5, 3)),  # codomain broadcast (commutativity)
-        ((5, 3), (5, None), (5, 3)),  # domain broadcast
-        ((5, None), (5, 3), (5, 3)),  # domain broadcast (commutativity)
-        ((5, None), (1, None), (5, None)),  # domain-agnostic
+class TestInferSumShape:
+    @pytest.mark.parametrize(
+        ["sh1", "sh2", "sh3"],
+        [
+            ((5, 3), (5, 3), (5, 3)),  # same shape
+            ((5, 3), (1, 3), (5, 3)),  # codomain broadcast
+            ((1, 3), (5, 3), (5, 3)),  # codomain broadcast (commutativity)
+            ((5, 3), (5, None), (5, 3)),  # domain broadcast
+            ((5, None), (5, 3), (5, 3)),  # domain broadcast (commutativity)
+            ((5, None), (1, None), (5, None)),  # domain-agnostic
+        ],
     )
-    for A, B, C in valid:
-        assert pycu.infer_sum_shape(A, B) == C
+    def test_valid(self, sh1, sh2, sh3):
+        assert pycu.infer_sum_shape(sh1, sh2) == sh3
 
-    invalid = (
-        ((None, 3), (5, 3)),  # codomain-agnostic
-        ((5, 3), (None, 1)),  # codomain-agnostic (commutativity)
-        ((5, 1), (5, 3)),  # domain broadcast
-        ((5, 2), (5, 3)),  # domain broadcast
-        ((5, 3), (2, 3)),  # codomain broadcast
-        ((5, None), (2, 3)),  # domain-agnostic broadcast
-        ((2, 3), (5, None)),  # domain-agnostic broadcast (commutativity)
+    @pytest.mark.parametrize(
+        ["sh1", "sh2"],
+        [
+            ((None, 3), (5, 3)),  # codomain-agnostic
+            ((5, 3), (None, 1)),  # codomain-agnostic (commutativity)
+            ((5, 1), (5, 3)),  # domain broadcast
+            ((5, 2), (5, 3)),  # domain broadcast
+            ((5, 3), (2, 3)),  # codomain broadcast
+            ((5, None), (2, 3)),  # domain-agnostic broadcast
+            ((2, 3), (5, None)),  # domain-agnostic broadcast (commutativity)
+        ],
     )
-    for A, B in invalid:
+    def test_invalid(self, sh1, sh2):
         with pytest.raises(ValueError):
-            pycu.infer_sum_shape(A, B)
+            pycu.infer_sum_shape(sh1, sh2)
 
 
-def test_infer_composition_shape():
-    valid = (
-        ((5, 3), (3, 4), (5, 4)),
-        ((5, None), (3, 4), (5, 4)),
-        ((5, 3), (3, None), (5, None)),
-        ((5, None), (3, None), (5, None)),
+class TestInferCompositionShape:
+    @pytest.mark.parametrize(
+        ["sh1", "sh2", "sh3"],
+        [
+            ((5, 3), (3, 4), (5, 4)),
+            ((5, None), (3, 4), (5, 4)),
+            ((5, 3), (3, None), (5, None)),
+            ((5, None), (3, None), (5, None)),
+        ],
     )
-    for A, B, C in valid:
-        assert pycu.infer_composition_shape(A, B) == C
+    def test_valid(self, sh1, sh2, sh3):
+        assert pycu.infer_composition_shape(sh1, sh2) == sh3
 
-    invalid = (
-        ((None, 3), (3, 4)),
-        ((5, 3), (None, 4)),
-        ((5, 3), (1, 4)),
-        ((5, 3), (1, None)),
+    @pytest.mark.parametrize(
+        ["sh1", "sh2"],
+        [
+            ((None, 3), (3, 4)),
+            ((5, 3), (None, 4)),
+            ((5, 3), (1, 4)),
+            ((5, 3), (1, None)),
+        ],
     )
-    for A, B in invalid:
+    def test_invalid(self, sh1, sh2):
         with pytest.raises(ValueError):
-            pycu.infer_composition_shape(A, B)
+            pycu.infer_composition_shape(sh1, sh2)
