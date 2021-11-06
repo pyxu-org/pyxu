e3bf4a607b38862719590980d5bfff9d4bae867c
Sepand KASHANI <kashani.sepand@gmail.com>
2021-11-06 13:48:00 +0100

    [test] Provide list of tested array backends.

--- src/pycsou/util/deps.py
+++ src/pycsou/util/deps.py
@@ -1,3 +1,24 @@
+import collections.abc as cabc
 import importlib.util
+import types
+
+import dask.array as da
+import numpy as np
 
 CUPY_ENABLED = importlib.util.find_spec("cupy") is not None
+
+
+def array_modules() -> cabc.Sequence[types.ModuleType]:
+    """
+    List of all (known) installed NumPy-compatible APIs.
+
+    This function is most useful for testing purposes.
+    """
+    xp = [np, da]
+
+    if CUPY_ENABLED:
+        import cupy as cp
+
+        xp.append(cp.ndarray)
+
+    return xp
