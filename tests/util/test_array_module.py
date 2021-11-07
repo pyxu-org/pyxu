75bed736e36ea2af1125668eae7b3f8fa371b4db
Sepand KASHANI <kashani.sepand@gmail.com>
2021-11-07 11:05:09 +0100

    [test] get_array_module().

---
+++ tests/util/test_array_module.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pytest
+
+import pycsou.util as pycu
+import pycsou.util.deps as pycd
+
+
+class TestGetArrayModule:
+    def test_array(self, xp):
+        x = xp.arange(5)
+        assert pycu.get_array_module(x) is xp
+
+    @pytest.mark.parametrize(
+        ["obj", "fallback", "fail"],
+        [
+            [None, None, True],
+            [None, np, False],
+            [1, None, True],
+            [1, np, False],
+        ],
+    )
+    def test_fallback(self, obj, fallback, fail):
+        # object is not an array type, so fail or return provided fallback
+        if not fail:
+            assert pycu.get_array_module(obj, fallback) is fallback
+        else:
+            with pytest.raises(ValueError):
+                assert pycu.get_array_module(obj, fallback)
