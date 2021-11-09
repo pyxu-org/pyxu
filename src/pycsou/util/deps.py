cda120fe1275fa762406f2621edad62da6443be6
Sepand KASHANI <kashani.sepand@gmail.com>
2021-11-09 13:34:13 +0100

    Redefinition of NDArray.
    "NDArray = numpy.typing.ArrayLike" is too broad for our usecase since we expect NDArray-abiding objects to already have a NumPy API.
    
    The redefinition here ensures we accept only NumPy-compatible arrays which are installed on the user's machine.

--- src/pycsou/util/deps.py
+++ src/pycsou/util/deps.py
@@ -1,6 +1,6 @@
-import collections.abc as cabc
+import functools
 import importlib.util
-import types
+import typing as typ
 
 import dask.array as da
 import numpy as np
@@ -8,17 +8,33 @@ import numpy as np
 CUPY_ENABLED = importlib.util.find_spec("cupy") is not None
 
 
-def array_modules() -> cabc.Sequence[types.ModuleType]:
+def array_backend_info():
     """
-    List of all (known) installed NumPy-compatible APIs.
+    List of all (known) installed NumPy-compatible (array, API) pairs.
 
     This function is most useful for testing purposes.
     """
-    xp = [np, da]
-
+    info = {
+        np.ndarray: np,
+        da.core.Array: da,
+    }
     if CUPY_ENABLED:
         import cupy as cp
 
-        xp.append(cp.ndarray)
+        info[cp.ndarray] = cp
+    return info
 
-    return xp
+
+def supported_array_types():
+    return tuple(array_backend_info().keys())
+
+
+def supported_array_modules():
+    return tuple(array_backend_info().values())
+
+
+NDArray = functools.reduce(
+    lambda a, b: typ.Union[a, b],
+    supported_array_types()[1:],
+    supported_array_types()[0],
+)
