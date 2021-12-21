362dbe8d30c0b55ef4a0a944ef14ab904f97c816
Sepand KASHANI <kashani.sepand@gmail.com>
2021-12-21 16:38:56 +0100

    [stop][BugFix] RelError: provide better docstring for callable.

--- src/pycsou/opt/stop.py
+++ src/pycsou/opt/stop.py
@@ -205,7 +205,8 @@ class RelError(pycs.StoppingCriterion):
             Variable in `Solver._mstate` to query.
         f: Callable
             Optional function to pre-apply to `Solver._mstate[var]` before applying the norm.
-            Defaults to the identity function.
+            Defaults to the identity function. The callable should have the same semantics as
+            `Property.apply`, or be a scalar->scalar map.
         norm: int | float
             Ln norm to use >= 0. (Default: L2.)
         satisfy_all: bool
