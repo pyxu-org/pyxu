1940e4f916e6b4b61faa8687e7cb0bcd24e70852
Sepand KASHANI <kashani.sepand@gmail.com>
2021-12-21 10:29:53 +0100

    [stop][MaxIter] Drop n=None support.
    This mode is not very user-discoverable.
    Will be replaced with the ManualStop() criteria.

--- src/pycsou/opt/stop.py
+++ src/pycsou/opt/stop.py
@@ -14,29 +14,23 @@ class MaxIter(pycs.StoppingCriterion):
     Stop iterative solver after a fixed number of iterations.
     """
 
-    def __init__(self, n: typ.Optional[int] = None):
+    def __init__(self, n: int):
         """
         Parameters
         ----------
-        n: int | None
+        n: int
             Max number of iterations allowed.
-            Defaults to infinity if unspecified, i.e. never halt.
         """
-        self._n = n
-        if n is not None:
-            try:
-                assert n > 0
-                self._n = int(n)
-            except:
-                raise ValueError(f"n: expected positive integer, got {n}.")
+        try:
+            assert int(n) > 0
+            self._n = int(n)
+        except:
+            raise ValueError(f"n: expected positive integer, got {n}.")
         self._i = 0
 
     def stop(self, state: cabc.Mapping) -> bool:
         self._i += 1
-        if self._n is None:
-            return False
-        else:
-            return self._i > self._n
+        return self._i > self._n
 
     def info(self) -> cabc.Mapping[str, float]:
         return dict(N_iter=self._i)
