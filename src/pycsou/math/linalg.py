4427c941814559d854d976c691d2525c6835a1e7
joanrue <joan.rue.q@gmail.com>
2022-03-08 18:21:36 +0100

    [hutch++][BugFix] use binomial dist. and default_rng

--- src/pycsou/math/linalg.py
+++ src/pycsou/math/linalg.py
@@ -1,11 +1,16 @@
 import warnings
 
+import numpy as np
+
+import pycsou.abc.operator as pyco
+import pycsou.linop.base as pycb
 import pycsou.runtime as pycrt
 import pycsou.util as pycu
+import pycsou.util.ptype as pyct
 
 
 @pycrt.enforce_precision(o=True)
-def hutchpp(linop, m: int = 4002, gpu: bool = False):
+def hutchpp(linop: pyco.LinOp, m: int = 126, xp: pyct.ArrayModule = np, seed: float = 0):
     r"""
     Computes an stocastic estimate of the trace for linear operators based on the Hutch++ algorithm (specifically,
     algorithm 3 of the paper https://arxiv.org/abs/2010.09649.
@@ -17,9 +22,12 @@ def hutchpp(linop, m: int = 4002, gpu: bool = False):
     m: int
         The number of queries desired to estimate the trace of the linear operator. ``m`` is set to 4002 by default,
         based on the analysis of variance described in theorem 10 of the Hutch++ paper. This default number of queries
-        corresponds to having an estimation error smaller than 0.01 with a probability of 0.9.
-    gpu: bool
-        If ``True`` the estimation of the trace is performed on the GPU.
+        corresponds to having an estimation error smaller than 0.1 with a probability of 0.9.
+    xp:  pycsou.util.ptype.ArrayModule
+         Which array module to use to represent the output.
+    seed: float
+        Seed for the random number generator.
+
 
     Returns
     -------
@@ -31,18 +39,25 @@ def hutchpp(linop, m: int = 4002, gpu: bool = False):
     This function calls Scipy’s function: :py:func:`scipy.sparse.linalg.qr`. See the documentation of this function
     for more information on its behaviour and the underlying LAPACK routines it relies on.
     """
-    if (
-        pycu.deps.CUPY_ENABLED and gpu
-    ):  # Scipy casts any input to the LinOp as a Numpy array so the cupyx version is needed.
-        import cupy as xp
-        from cupy.linalg import qr
 
-        mode = "reduced"
+    if xp == np:
+        kwargs = {"mode": "economic"}
+        import scipy
+
+        xlin = scipy.linalg
+    elif pycu.deps.CUPY_ENABLED:  # Scipy casts any input to the LinOp as a Numpy array so the cupyx version is needed.
+        import cupy as cp
+
+        if xp == cp:
+            xlin = cp.linalg
+            kwargs = {"mode": "reduced"}
     else:
-        import numpy as xp
-        from scipy.linalg import qr
+        import dask.array as da
 
-        mode = "economic"
+        if xp == da:
+            xlin = da
+            kwargs = {}
+
     d = linop.shape[1]
     if m >= d:
         warnings.warn(
@@ -52,8 +67,18 @@ def hutchpp(linop, m: int = 4002, gpu: bool = False):
         )
         return xp.trace(linop.apply(xp.eye(d)))
 
-    s = xp.random.randn(d, (m + 2) // 4)
-    g = xp.random.randn(d, (m - 2) // 2)
-    q, _ = qr(linop.apply(s.T).T, mode=mode)
+    if isinstance(linop, pycb.ExplicitLinOp):
+        if xp != pycu.get_array_module(linop.mat):
+            warnings.warn(
+                f"The array module of the :py:class:`~pycsou.linop.base.ExplicitLinOp` "
+                f"({pycu.get_array_module(linop.mat)}) and the requested array module "
+                f"({xp}) are different.",
+                UserWarning,
+            )
+
+    rng = xp.random.default_rng(seed=seed)
+    s = rng.standard_normal(size=(d, (m + 2) // 4))
+    g = rng.binomial(n=1, p=0.5, size=(d, (m - 2) // 2)) * 2 - 1
+    q, _ = xlin.qr(linop.apply(s.T).T, **kwargs)
     proj = g - q @ (q.T @ g)
     return xp.trace(q.T @ linop.apply(q.T).T) + (2.0 / (m - 2)) * xp.trace(proj.T @ linop.apply(proj.T).T)
