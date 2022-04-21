df3660ea8024595a1237f333ae3b0445a16d1d12
okumuskaan <okumuskaan@hotmail.com>
2022-04-21 07:08:33 +0200

    [testing] update test_ufunc.py
    Implemented testing of exponential and logarithm functions

--- src/pycsou_tests/operator/examples/test_ufunc.py
+++ src/pycsou_tests/operator/examples/test_ufunc.py
@@ -556,3 +556,189 @@ class TestArctanh(conftest.MapT):
         return np.clip(self._random_array((N_test, data_shape[0])), a_min=-1+0.01, a_max=1-0.01)
 
 
+# Exponentials and logarithms
+
+class Exp(pyca.DiffMap):
+    def __init__(self, shape: pyct.Shape):
+        super(Exp, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.exp(arr)
+
+    def jacobian(self, arr: pyct.NDArray):
+        xp = pycu.get_array_module(arr)
+        return pyclb.ExplicitLinOp(xp.diag(xp.exp(arr)))
+
+class TestExp(conftest.DiffMapT):
+    @pytest.fixture
+    def dim(self):
+        return 10
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Exp(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(-4, 4, data_shape[0])
+        B = np.exp(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+
+class Log(pyca.DiffMap):
+    def __init__(self, shape: pyct.Shape):
+        super(Log, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.log(arr)
+
+    def jacobian(self, arr: pyct.NDArray):
+        xp = pycu.get_array_module(arr)
+        return pyclb.ExplicitLinOp(xp.diag(1/arr))
+
+class TestLog(conftest.DiffMapT):
+    @pytest.fixture
+    def dim(self):
+        return 100
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Log(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(0.1, 10, data_shape[0])
+        print("A: ", A)
+        B = np.log(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return np.abs(self._random_array((N_test, data_shape[0])))
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, data_shape):
+        N_test = 5
+        return np.abs(self._random_array((N_test, data_shape[0])))
+
+
+class Log10(pyca.DiffMap):
+    def __init__(self, shape: pyct.Shape):
+        super(Log10, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.log10(arr)
+
+    def jacobian(self, arr: pyct.NDArray):
+        xp = pycu.get_array_module(arr)
+        return pyclb.ExplicitLinOp(xp.diag(1/(xp.log(10)*arr)))
+
+class TestLog10(conftest.DiffMapT):
+    @pytest.fixture
+    def dim(self):
+        return 100
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Log10(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(0.1, 10, data_shape[0])
+        B = np.log10(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return np.abs(self._random_array((N_test, data_shape[0])))
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, data_shape):
+        N_test = 5
+        return np.abs(self._random_array((N_test, data_shape[0])))
+
+
+class Log2(pyca.DiffMap):
+    def __init__(self, shape: pyct.Shape):
+        super(Log2, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.log2(arr)
+
+    def jacobian(self, arr: pyct.NDArray):
+        xp = pycu.get_array_module(arr)
+        return pyclb.ExplicitLinOp(xp.diag(1/(xp.log(2)*arr)))
+
+class TestLog2(conftest.DiffMapT):
+    @pytest.fixture
+    def dim(self):
+        return 100
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Log2(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(0.1, 10, data_shape[0])
+        B = np.log2(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return np.abs(self._random_array((N_test, data_shape[0])))
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, data_shape):
+        N_test = 5
+        return np.abs(self._random_array((N_test, data_shape[0])))
+
