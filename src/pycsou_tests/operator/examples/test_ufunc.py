771de5c03bcc934d379a170042955779e7b12ae8
okumuskaan <okumuskaan@hotmail.com>
2022-04-21 07:06:37 +0200

    [testing] update test_ufunc.py
    Implemented testing of tan, arcsin, arccos, arctan

--- src/pycsou_tests/operator/examples/test_ufunc.py
+++ src/pycsou_tests/operator/examples/test_ufunc.py
@@ -104,3 +104,184 @@ class TestCos(conftest.DiffMapT):
         N_test = 5
         return self._random_array((N_test, data_shape[0]))
 
+
+class Tan(pyca.Map):
+    def __init__(self, shape: pyct.Shape):
+        super(Tan, self).__init__(shape)
+
+    @pycrt.enforce_precision(i="arr", o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.tan(arr)
+
+class TestTan(conftest.MapT):
+    """
+    Tangent function diverges for :math:`\mp(2k+1)k\pi/2`, with
+    :math:`k \in \mathbb{N}`. Testing is done on :math`[-3*\pi/2+0.2,
+    -\pi/2-0.2] \cup [-\pi/2+0.2, \pi/2-0.2] \cup [\pi/2+0.2,
+    3*\pi/2-0.2]`.
+    """
+    @pytest.fixture
+    def dim(self):
+        return 7
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Tan(shape=data_shape)
+
+    @pytest.fixture(
+        params=[
+            dict(
+                in_=dict(arr=np.linspace(-3*np.pi / 2 + 0.2, -np.pi / 2 - 0.2, 5)),
+                out=np.tan(np.linspace(-3*np.pi / 2 + 0.2, -np.pi / 2 - 0.2, 5))
+            ),
+            dict(
+                in_=dict(arr=np.linspace(-np.pi/2+0.1, np.pi/2-0.1, 5)),
+                out=np.tan(np.linspace(-np.pi/2+0.1, np.pi/2-0.1, 5))
+            ),
+            dict(
+                in_=dict(arr=np.linspace(np.pi / 2 + 0.2, 3 * np.pi / 2 - 0.2, 5)),
+                out=np.tan(np.linspace(np.pi / 2 + 0.2, 3 * np.pi / 2 - 0.2, 5))
+            )
+        ]
+    )
+    def data_apply(self, request):
+        return request.param
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+
+class Arcsin(pyca.Map):
+    def __init__(self, shape: pyct.Shape):
+        super(Arcsin, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.arcsin(arr)
+
+class TestArcsin(conftest.MapT):
+    """
+    Inverse sine function defined for :math:`[-1,1]`.
+    """
+    @pytest.fixture
+    def dim(self):
+        return 7
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Arcsin(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(-1, 1, data_shape[0])
+        B = np.arcsin(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return np.clip(self._random_array((N_test, data_shape[0])), -1, 1)
+
+
+class Arccos(pyca.Map):
+    def __init__(self, shape: pyct.Shape):
+        super(Arccos, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.arccos(arr)
+
+class TestArccos(conftest.MapT):
+    """
+    Inverse cosine function defined for :math:`[-1,1]`.
+    """
+    @pytest.fixture
+    def dim(self):
+        return 7
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Arccos(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(-1, 1, data_shape[0])
+        B = np.arccos(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return np.clip(self._random_array((N_test, data_shape[0])), -1, 1)
+
+
+class Arctan(pyca.DiffMap):
+    def __init__(self, shape: pyct.Shape):
+        super(Arctan, self).__init__(shape)
+        self._lipschitz = 1
+        self._diff_lipschitz = 0.65
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.arctan(arr)
+
+    def jacobian(self, arr: pyct.NDArray):
+        xp = pycu.get_array_module(arr)
+        return pyclb.ExplicitLinOp(xp.diag(1/(1 + xp.power(arr, 2))))
+
+class TestArctan(conftest.DiffMapT):
+    @pytest.fixture
+    def dim(self):
+        return 10
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Arctan(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(-100, 100, data_shape[0])
+        B = np.arctan(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
