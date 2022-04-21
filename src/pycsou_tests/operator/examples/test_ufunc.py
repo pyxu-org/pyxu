17119460a3c8b7d380e0162d264e4f1e518c7ebc
okumuskaan <okumuskaan@hotmail.com>
2022-04-21 07:07:43 +0200

    [testing] update test_ufunc.py
    Implemented testing of hyperbolic functions

--- src/pycsou_tests/operator/examples/test_ufunc.py
+++ src/pycsou_tests/operator/examples/test_ufunc.py
@@ -285,3 +285,274 @@ class TestArctan(conftest.DiffMapT):
         N_test = 5
         return self._random_array((N_test, data_shape[0]))
 
+
+# Hyperbolic Functions
+
+class Sinh(pyca.DiffMap):
+    def __init__(self, shape: pyct.Shape):
+        super(Sinh, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.sinh(arr)
+
+    def jacobian(self, arr: pyct.NDArray):
+        xp = pycu.get_array_module(arr)
+        return pyclb.ExplicitLinOp(xp.diag(xp.cosh(arr)))
+
+class TestSinh(conftest.DiffMapT):
+    @pytest.fixture
+    def dim(self):
+        return 100
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Sinh(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(-3, 3, data_shape[0])
+        B = np.sinh(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+
+class Cosh(pyca.DiffMap):
+    def __init__(self, shape: pyct.Shape):
+        super(Cosh, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.cosh(arr)
+
+    def jacobian(self, arr: pyct.NDArray):
+        xp = pycu.get_array_module(arr)
+        return pyclb.ExplicitLinOp(xp.diag(xp.sinh(arr)))
+
+class TestCosh(conftest.DiffMapT):
+    @pytest.fixture
+    def dim(self):
+        return 100
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Cosh(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(-3, 3, data_shape[0])
+        B = np.cosh(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+
+class Tanh(pyca.DiffMap):
+    def __init__(self, shape: pyct.Shape):
+        super(Tanh, self).__init__(shape)
+        self._lipschitz = 1
+        self._diff_lipschitz = 0.77
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.tanh(arr)
+
+    def jacobian(self, arr: pyct.NDArray):
+        xp = pycu.get_array_module(arr)
+        return pyclb.ExplicitLinOp(xp.diag(1/(xp.power(xp.cosh(arr), 2))))
+        
+class TestTanh(conftest.DiffMapT):
+    @pytest.fixture
+    def dim(self):
+        return 100
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Tanh(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(-3, 3, data_shape[0])
+        B = np.tanh(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+
+class Arcsinh(pyca.DiffMap):
+    def __init__(self, shape: pyct.Shape):
+        super(Arcsinh, self).__init__(shape)
+        self._lipschitz = 1
+        self._diff_lipschitz = 0.39
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.arcsinh(arr)
+
+    def jacobian(self, arr: pyct.NDArray):
+        xp = pycu.get_array_module(arr)
+        return pyclb.ExplicitLinOp(xp.diag(1/(xp.sqrt(xp.power(arr, 2) + 1))))
+
+class TestArcsinh(conftest.DiffMapT):
+    @pytest.fixture
+    def dim(self):
+        return 100
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Arcsinh(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(-10, 10, data_shape[0])
+        B = np.arcsinh(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, data_shape):
+        N_test = 5
+        return self._random_array((N_test, data_shape[0]))
+
+
+class Arccosh(pyca.Map):
+    def __init__(self, shape: pyct.Shape):
+        super(Arccosh, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.arccosh(arr)
+
+class TestArccosh(conftest.MapT):
+    """
+    Inverse hyperbolic cosine function defined for :math:`[1,\infty)`.
+    """
+    @pytest.fixture
+    def dim(self):
+        return 7
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Arccosh(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(1, 5, data_shape[0])
+        B = np.arccosh(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return np.clip(self._random_array((N_test, data_shape[0]))+3, a_min=1, a_max=4)
+
+
+class Arctanh(pyca.Map):
+    def __init__(self, shape: pyct.Shape):
+        super(Arctanh, self).__init__(shape)
+
+    @pycrt.enforce_precision(i='arr', o=True)
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.arctanh(arr)
+
+class TestArctanh(conftest.MapT):
+    """
+    Inverse hyperbolic tangent function defined for :math:`(-1,1)`.
+    """
+    @pytest.fixture
+    def dim(self):
+        return 7
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def op(self, data_shape):
+        return Arctanh(shape=data_shape)
+
+    @pytest.fixture
+    def data_apply(self, data_shape):
+        A = np.linspace(-1+0.01, 1-0.01, data_shape[0])
+        B = np.arctanh(A)
+        return dict(
+            in_=dict(arr=A),
+            out=B,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, data_shape):
+        N_test = 5
+        return np.clip(self._random_array((N_test, data_shape[0])), a_min=-1+0.01, a_max=1-0.01)
+
+
