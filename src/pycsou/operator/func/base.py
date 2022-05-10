89456c22b55452658c8e2d140f4e2e4f3e2716a0
MatthieuMeo <matthieu.simeoni@gmail.com>
2022-05-10 21:17:40 +0200

    [func] Bug fixes and small improvements to QuadraticFunc.

--- src/pycsou/operator/func/base.py
+++ src/pycsou/operator/func/base.py
@@ -4,33 +4,38 @@ import warnings
 import numpy as np
 
 import pycsou.abc.operator as pyco
+import pycsou.operator.linop as pycl
 import pycsou.runtime as pycrt
 import pycsou.util.ptype as pyct
 
 
 class QuadraticFunc(pyco.ProxDiffFunc):
     r"""
-    Build a quadratic functional of the form:
+    Quadratic functional.
+
+    The quadratic functional is defined as:
 
     .. math::
-        f(\mathbf{x})=<\mathbf{x}, \mathbf{Q}\mathbf{x}> + \mathbf{c}^T\mathbf{x} + t, \qquad \forall \mathbf{x}\in\mathbb{R}^N,
+        f(\mathbf{x})= \frac{1}{2} <\mathbf{x}, \mathbf{Q}\mathbf{x}> + \mathbf{c}^T\mathbf{x} + t, \qquad \forall \mathbf{x}\in\mathbb{R}^N,
 
-    From a given self-adjoint, positive operator :math:`\mathbf{Q}:\mathbb{R}^N\rightarrow \mathbb{R}^N`, a vector
+    From a given self-adjoint, positive, nonzero, operator :math:`\mathbf{Q}:\mathbb{R}^N\rightarrow \mathbb{R}^N`, a vector
     :math:`\mathbf{c}\in\mathbb{R}^N` and a scalar :math:`t`.
 
-    with gradient given by:
+    Its gradient is given by:
+
     .. math::
-        \nabla_{f}(\mathbf{x}) = \mathbf{Q}\mathbf{x} + \mathbf{c}
+        \nabla f(\mathbf{x}) = \mathbf{Q}\mathbf{x} + \mathbf{c}
 
-    and proximity operator given by:
+    and proximity operator:
+
     .. math::
-        prox_{\tau f}(x) = (\mathbf{Q} + \frac{1}{tau} \mathcal{I})^{-1} ( \frac{\mathbf{x}}{tau} - \mathbf{c})
+        prox_{\tau f}(x) = \left(\mathbf{Q} + \frac{1}{tau} \mathcal{I}\right)^{-1} \left( \frac{\mathbf{x}}{tau} - \mathbf{c}\right).
 
-    Which in practice is solved via the conjugate gradient method (:py:class:`pycsou.opt.solver.cg`)
+    In practice, the proximity operator is computed via conjugate gradient (:py:class:`pycsou.opt.solver.cg`).
 
-    The lipschitz constant (`_lipschitz`) of a quadratic functional is unbounded when the domain of :math:`\mathbf{Q}`, is not bounded,
-    and thus, it is set to infinity. The lipschitz constant of the gradient (`_diff_lipschitz`) is given by the operator norm of
-    :math:`\mathbf{Q}`.
+    The lipschitz constant (``_lipschitz``) of a quadratic functional is unbounded when the domain of :math:`\mathbf{Q}`, is not bounded,
+    and thus, it is set to infinity. The lipschitz constant of the gradient (``_diff_lipschitz``) is given by
+    the spectral norm of :math:`\mathbf{Q}`.
 
     Examples
     --------
@@ -40,7 +45,7 @@ class QuadraticFunc(pyco.ProxDiffFunc):
     >>> mat = np.arange(10).reshape(2,5)
     >>> A = ExplicitLinOp(mat)
     >>> b = np.arange(5).reshape(1,5)
-    >>> LeastSquares = QuadraticFunc(Q=A.gram(), c=A.adjoint(b), t=b.dot(b))
+    >>> LeastSquares = 2 * QuadraticFunc(Q=A.gram(), c= -A.adjoint(b), t=(1/2) * b.dot(b))
 
     Notes
     -----
@@ -55,7 +60,6 @@ class QuadraticFunc(pyco.ProxDiffFunc):
 
     """
 
-    @pycrt.enforce_precision(i="vec")
     def __init__(
         self, Q: pyco.SelfAdjointOp, c: pyct.NDArray, t: typ.Optional[pyct.Real] = None, enable_warnings: bool = True
     ):
@@ -64,7 +68,7 @@ class QuadraticFunc(pyco.ProxDiffFunc):
         Parameters
         ----------
         Q: Self-adjoint positive linear operator
-            Self-adjoint, positive, nonzero, bounded linear operator
+            Self-adjoint, positive, nonzero, linear operator.
         c: NDArray
             (N,) vector. N-D input arrays are flattened. This is the vectorial representation of the linear term of the
              quadratic functional.
@@ -73,27 +77,28 @@ class QuadraticFunc(pyco.ProxDiffFunc):
         enable_warnings: bool
             If ``True``, the user will be warned in case of mismatching precision issues.
         """
-        super(QuadraticFunc, self).__init__(shape=(1, vec.size))
+        super(QuadraticFunc, self).__init__(shape=(1, c.size))
         self.Q = Q
-        self.c = c
-        self.t = t
+        self.c = pycrt.coerce(c)
+        self.t = pycrt.coerce(t)
         self.enable_warnings = enable_warnings
 
         self._lipschitz = np.infty
+        self._diff_lipschitz = self.Q._lipschitz
 
     @pycrt.enforce_precision(i="arr")
     def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
         if (
-            not all([(elem.dtype == pycrt.getPrecision().value) or (elem is None) for elem in [self.Q, self.c, self.t]])
+            not all([(elem.dtype == pycrt.getPrecision().value) or (elem is None) for elem in [self.c, self.t]])
             and self._enable_warnings
         ):
             warnings.warn("Computation may not be performed at the requested precision.", UserWarning)
-        return self.Q(arr).dot(arr) + self.c(arr) + self.t
+        return (self.Q(arr) * arr).sum(axis=-1, keepdims=True) / 2 + self.c(arr) + self.t
 
     @pycrt.enforce_precision(i="arr")
     def grad(self, arr: pyct.NDArray) -> pyct.NDArray:
         if (
-            not all([(elem.dtype == pycrt.getPrecision().value) or (elem is None) for elem in [self.Q, self.c, self.t]])
+            not all([(elem.dtype == pycrt.getPrecision().value) or (elem is None) for elem in [self.c, self.t]])
             and self._enable_warnings
         ):
             warnings.warn("Computation may not be performed at the requested precision.", UserWarning)
@@ -102,11 +107,8 @@ class QuadraticFunc(pyco.ProxDiffFunc):
     @pycrt.enforce_precision(i=["arr", "tau"])
     def prox(self, arr: pyct.NDArray, tau: pyct.Real) -> pyct.NDArray:
         if (
-            not all([(elem.dtype == pycrt.getPrecision().value) or (elem is None) for elem in [self.Q, self.c, self.t]])
+            not all([(elem.dtype == pycrt.getPrecision().value) or (elem is None) for elem in [self.c, self.t]])
             and self._enable_warnings
         ):
             warnings.warn("Computation may not be performed at the requested precision.", UserWarning)
-        return pycs.cg.CG(Q + tau * IdentityOp(Q.shape)).fit(arr - self.c)
-
-    def dif_lipschitz(self, **kwargs) -> float:
-        return self.Q.lipschitz()
+        return pycs.cg.CG(self.Q + (1 / tau) * pycl.IdentityOp(self.Q.shape)).fit(arr / tau - self.c)
