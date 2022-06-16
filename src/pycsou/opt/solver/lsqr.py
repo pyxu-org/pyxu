c4ae707b9ec67bec45b6da59fbecd4b541090fef
okumuskaan <okumuskaan@hotmail.com>
2022-06-16 14:04:20 +0200

    [opt/solver/lsqr] Document and update lsqr.py
    - Final documentation is done.
    - The function is made to work for multiple inputs by parallel processing.
    - Stopping criterion is updated.
    - The function is made to respond to trivial solutions.

--- src/pycsou/opt/solver/lsqr.py
+++ src/pycsou/opt/solver/lsqr.py
@@ -1,3 +1,9 @@
+# #############################################################################
+# lsqr.py
+# ========
+# Author : Kaan Okumus [okukaan@gmail.com]
+# #############################################################################
+
 import typing as typ
 
 import numpy as np
@@ -12,67 +18,72 @@ import pycsou.util.ptype as pyct
 
 class LSQR(pycs.Solver):
     r"""
-     LSQR Method.
+    LSQR Method.
 
-     The LSQR method solves the system of linear equations :math:`\mathbf{A}\mathbf{x}=\mathbf{b}` iteratively.
-     :math:`\mathbf{A}` is a rectangular matrix of dimension m-by-n, where all cases are allowed: m=n, m>n or m<n.
-     :math:`\mathbf{b}` is a vector of length m. The matrix :math:`\mathbf{A}` may be square or rectangular and may have
-     any rank.
-     For unsymmetric equations, it solves :math:`\mathbf{A}\mathbf{x}=\mathbf{b}`.
-     For linear least squares, it solves :math:`||\mathbf{b}-\mathbf{A}\mathbf{x}||^2`.
-     For damped least squares, it solves :math:`||\mathbf{b}-\mathbf{A}\mathbf{x}||^2 + d^2 ||\mathbf{x}-\mathbf{x_0}||
-     ^2`.
+    The LSQR method solves the system of linear equations :math:`\mathbf{A}\mathbf{x}=\mathbf{b}` iteratively.
+    :math:`\mathbf{A}` is a rectangular matrix of dimension m-by-n, where all cases are allowed: m=n, m>n or m<n.
+    :math:`\mathbf{b}` is a vector of length m. The matrix :math:`\mathbf{A}` may be square or rectangular and may have
+    any rank.
+
+    * For unsymmetric equations, it solves :math:`\mathbf{A}\mathbf{x}=\mathbf{b}`.
+
+    * For linear least squares, it solves :math:`||\mathbf{b}-\mathbf{A}\mathbf{x}||^2`.
+
+    * For damped least squares, it solves :math:`||\mathbf{b}-\mathbf{A}\mathbf{x}||^2 + d^2 ||\mathbf{x}-\mathbf{x_0}||^2`.
 
 
-     ``CG()`` **Parameterization**
-     A: pycsou.abc.operator.PosDefOp
+    ``LSQR()`` **Parameterization**
+
+    A: :py:class:`pycsou.abc.operator.PosDefOp`
         Any positive definite linear operator is accepted.
-     damp: float
+    damp: float
         Damping coefficient. Default is 0.
-     atol, btol: float
+    atol, btol: float
         Stopping tolerances. LSQR continues iterations until a certain backward error estimate is smaller than some
         quantity depending on atol and btol. Default is 1e-6 for both.
-     conlim: float
-        LSQR terminates if an estimate :math:`cond(\mathbf{A})` exceeds conlim. Default is 1e8.
-     iter_lim: int, optional
+    conlim: float
+        LSQR terminates if an estimate :math:`\text{cond}(\mathbf{A})` exceeds conlim. Default is 1e8.
+    iter_lim: int, optional
         LSQR terminates if the number of iterations reaches iter_lim. Default is None.
 
-     ``CG.fit()`` **Parameterization**
+    ``LSQR.fit()`` **Parameterization**
 
-     b: NDArray
+    b: NDArray
          (..., N) 'b' terms in the LSQR cost function. All problems are solved in parallel.
-     x0: NDArray
+    x0: NDArray
         (..., N) initial point(s). Defaults to 0 if unspecified.
-     restart_rate: int
+    restart_rate: int
         Number of iterations after which restart is applied. By default, restart is done after 'n' iterations, where 'n'
         corresponds to the dimension of the linear operator :math:`\mathbf{A}`.
 
-    **Remark:** In pycsou.opt.solver.stop, StopCriterion_LSQR is developed for the stopping criterion of LSQR. For
+    **Remark:** :py:class:`pycsou.opt.solver.stop.StopCriterion_LSQR` is developed for the stopping criterion of LSQR. For
     computational speed, explicit norms were not computated. Instead, their estimation was used, which is referred from
-    [1].
+    [1]_.
 
-     References
-     ----------
-        [1] S.-C. Choi, "Iterative Methods for Singular Linear Equations
-            and Least-Squares Problems", Dissertation,
-            http://www.stanford.edu/group/SOL/dissertations/sou-cheng-choi-thesis.pdf
-     Examples
-     --------
-     >>> import numpy as np
+    Examples
+    --------
+    >>> import numpy as np
      >>> from pycsou.abc import LinOp
-     >>> # Create a PSD linear operator
-     >>> rng = np.random.default_rng(seed=0)
-     >>> mat = rng.normal(size=(10, 10))
-     >>> A = LinOp.from_array(mat).gram()
-     >>> # Create the ground truth 'x_star'
-     >>> x_star = rng.normal(size=(2, 2, 10))
-     >>> # Generate the corresponding data vector 'b'
-     >>> b = A.apply(x_star)
-     >>> # Solve 'Ax=b' for 'x' with the conjugate gradient method
-     >>> lsqr = LSQR(A, show_progress=False)
-     >>> lsqr.fit(b=b)
-     >>> x_solution = lsqr.solution()
-     >>> assert np.allclose(x_star, x_solution)
+    >>> # Create a PSD linear operator
+    >>> rng = np.random.default_rng(seed=0)
+    >>> mat = rng.normal(size=(10, 10))
+    >>> A = LinOp.from_array(mat).gram()
+    >>> # Create the ground truth 'x_star'
+    >>> x_star = rng.normal(size=(2, 2, 10))
+    >>> # Generate the corresponding data vector 'b'
+    >>> b = A.apply(x_star)
+    >>> # Solve 'Ax=b' for 'x' with the conjugate gradient method
+    >>> lsqr = LSQR(A, show_progress=False)
+    >>> lsqr.fit(b=b)
+    >>> x_solution = lsqr.solution()
+    >>> assert np.allclose(x_star, x_solution)
+    True
+
+    **References:**
+
+    .. [1] S.-C. Choi, "Iterative Methods for Singular Linear Equations
+        and Least-Squares Problems", Dissertation,
+        http://www.stanford.edu/group/SOL/dissertations/sou-cheng-choi-thesis.pdf
     """
 
     def __init__(
@@ -116,20 +127,25 @@ class LSQR(pycs.Solver):
         x0: typ.Optional[pyct.NDArray] = None,
     ):
         xp = pycu.get_array_module(b)
+
+        b = xp.atleast_1d(b)
+        if b.ndim > 1:
+            b = b.squeeze()
+        m, n = self._A.shape
+
         mst = self._mstate
-        mst["anorm"] = mst["acond"] = mst["res2"] = mst["xnorm"] = mst["xxnorm"] = 0
-        mst["ddnorm"] = mst["z"] = mst["sn2"] = 0
-        mst["cs2"] = -1
+        mst["normA"] = mst["condA"] = mst["res2"] = mst["xnorm"] = mst["xxnorm"] = 0.0
+        mst["ddnorm"] = mst["z"] = mst["sn2"] = 0.0
+        mst["cs2"] = -1.0
 
         u = b
         bnorm = xp.linalg.norm(b)
 
         if x0 is None:
-            # TODO: make sure shape is correct
-            x = xp.zeros((self._A.shape[1],))
+            x = xp.zeros(n)
             beta = bnorm.copy()
         else:
-            x = x0
+            x = xp.asarray(x0)
             u = u - self._A.apply(x)
             beta = xp.linalg.norm(u)
 
@@ -144,13 +160,22 @@ class LSQR(pycs.Solver):
         if alpha > 0:
             v = (1 / alpha) * v
 
+        arnorm = alpha * beta
+        if arnorm == 0:
+            mst["trivial"] = True
+            if b.ndim == 1:
+                mst["x"] = xp.zeros(n)
+            elif b.ndim == 2:
+                mst["x"] = xp.zeros((b.shape[0], n))
+            return
+
         mst["x"] = x
         mst["u"], mst["v"] = u, v
         mst["w"] = v.copy()
         mst["rhobar"] = mst["alpha"] = alpha
-        mst["phibar"] = mst["rnorm"] = mst["r1norm"] = mst["r2norm"] = beta
-        mst["arnorm"], mst["bnorm"] = alpha * beta, bnorm
-        mst["itn"], mst["istop"] = 0, None
+        mst["phibar"] = mst["rnorm"] = mst["normr1"] = mst["normr2"] = beta
+        mst["bnorm"] = bnorm
+        mst["trivial"] = False
         mst["test1"], mst["test2"] = 1.0, alpha / beta
         mst["test3"] = mst["t1"] = mst["rtol"] = None
 
@@ -178,10 +203,13 @@ class LSQR(pycs.Solver):
     def m_step(self):
 
         mst = self._mstate  # shorthand
+        if mst["trivial"]:
+            return
         xp = pycu.get_array_module(mst["x"])
+
         x, u, v, w = mst["x"], mst["u"], mst["v"], mst["w"]
         alpha, rhobar, phibar = mst["alpha"], mst["rhobar"], mst["phibar"]
-        anorm, ddnorm, xxnorm, bnorm = mst["anorm"], mst["ddnorm"], mst["xxnorm"], mst["bnorm"]
+        normA, ddnorm, xxnorm, bnorm = mst["normA"], mst["ddnorm"], mst["xxnorm"], mst["bnorm"]
         res2, sn2, cs2, z = mst["res2"], mst["sn2"], mst["cs2"], mst["z"]
 
         # Bidiagonalizaion to obtain next beta, u, alpha, v
@@ -190,7 +218,7 @@ class LSQR(pycs.Solver):
 
         if beta > 0:
             u = (1 / beta) * u
-            anorm = xp.sqrt(anorm**2 + alpha**2 + beta**2 + self._dampsq)
+            normA = xp.sqrt(normA**2 + alpha**2 + beta**2 + self._dampsq)
             v = self._A.T.apply(u) - beta * v
             alpha = xp.linalg.norm(v)
             if alpha > 0:
@@ -239,7 +267,7 @@ class LSQR(pycs.Solver):
         z = rhs / gamma
         xxnorm = xxnorm + z**2
 
-        acond = anorm * xp.sqrt(ddnorm)
+        condA = normA * xp.sqrt(ddnorm)
         res1 = phibar**2
         res2 = res2 + psi**2
         rnorm = xp.sqrt(res1 + res2)
@@ -248,29 +276,30 @@ class LSQR(pycs.Solver):
         # Distinguish residual norms
         if self._damp > 0:
             r1sq = rnorm**2 - self._dampsq * xxnorm
-            r1norm = xp.sqrt(xp.abs(r1sq))
+            normr1 = xp.sqrt(xp.abs(r1sq))
             if r1sq < 0:
-                r1norm = -r1norm
+                normr1 = -normr1
         else:
-            r1norm = rnorm
-        r2norm = rnorm
+            normr1 = rnorm
+        normr2 = rnorm
 
         # Get necessary metrics for convergence test
         test1 = rnorm / bnorm
-        test2 = arnorm / (anorm * rnorm + self._eps)
-        test3 = 1 / (acond + self._eps)
-        t1 = test1 / (1 + anorm * xnorm / bnorm)
-        rtol = self._btol + self._atol * anorm * xnorm / bnorm
+        test2 = arnorm / (normA * rnorm + self._eps)
+        test3 = 1 / (condA + self._eps)
+        t1 = test1 / (1 + normA * xnorm / bnorm)
+        rtol = self._btol + self._atol * normA * xnorm / bnorm
 
         # Store necessary parameters
-        mst["anorm"], mst["ddnorm"], mst["xnorm"], mst["xxnorm"], mst["bnorm"] = anorm, ddnorm, xnorm, xxnorm, bnorm
+        mst["normA"], mst["ddnorm"], mst["xnorm"], mst["xxnorm"], mst["bnorm"] = normA, ddnorm, xnorm, xxnorm, bnorm
         mst["res2"], mst["sn2"], mst["cs2"], mst["z"] = res2, sn2, cs2, z
         mst["test1"], mst["test2"], mst["test3"] = test1, test2, test3
         mst["t1"], mst["rtol"] = t1, rtol
-        mst["r1norm"], mst["r2norm"], mst["acond"] = r1norm, r2norm, acond
+        mst["normr1"], mst["normr2"], mst["condA"] = normr1, normr2, condA
 
     def default_stop_crit(self) -> pycs.StoppingCriterion:
-        stop_crit = pycos.StopCriterion_LSQR(
+        stop_crit = pycos.StopCriterion_LSQMR(
+            method="lsqr",
             atol=self._atol,
             ctol=self._ctol,
             itn=0,
