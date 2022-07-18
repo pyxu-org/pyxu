a5707a09cef4ad9559bad0407f0ee2d9f7f6cc08
Sepand KASHANI <kashani.sepand@gmail.com>
2022-07-18 15:43:31 +0200

    [operator.func] Formatting/Cleanup.
    QuadraticFunc arithmetic removed. (Will be moved to dedicated arithmetic module.)

--- src/pycsou/operator/func/__init__.py
+++ src/pycsou/operator/func/__init__.py
@@ -1,25 +1,3 @@
-import pycsou.util.ptype as pyct
-
-
-def shift_loss(
-    op: pyct.MapT,
-    data: pyct.NDArray = None,
-) -> pyct.MapT:
-    """
-    Shift a functional into a loss functional.
-
-    Parameters
-    ----------
-    data: pyct.NDArray
-        (N,) input data.
-
-    Returns
-    -------
-    op: pyct.MapT
-        Loss functionial.
-        If `data = None`, then this function is a no-op.
-    """
-    if data is None:
-        return op
-    else:
-        return op.argshift(-data)
+from pycsou.operator.func.loss import *
+from pycsou.operator.func.quadratic import *
+from pycsou.operator.linop import NullFunc  # alias import
