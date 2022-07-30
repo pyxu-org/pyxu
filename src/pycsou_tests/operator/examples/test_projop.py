3c228ff29685fd61dfa535b00145ffff6133e29f
Sepand KASHANI <kashani.sepand@gmail.com>
2022-07-30 18:38:42 +0200

    [test][operator] Replace CabinetProjection with Oblique to test D>3 case.

--- src/pycsou_tests/operator/examples/test_projop.py
+++ src/pycsou_tests/operator/examples/test_projop.py
@@ -7,57 +7,49 @@ import pycsou.util as pycu
 import pycsou_tests.operator.conftest as conftest
 
 
-class CabinetProjection(pyca.ProjOp):
-    # https://en.wikipedia.org/wiki/Oblique_projection#Cabinet_projection
-    def __init__(self, angle: float):
-        super().__init__(shape=(3, 3))
-        self._A = np.array(
-            [
-                [1, 0, np.cos(angle) / 2],
-                [0, 1, np.sin(angle) / 2],
-                [0, 0, 0],
-            ]
-        )
-
-    @staticmethod
-    def _bcast_apply(A, b):
-        xp = pycu.get_array_module(A)
-        A = xp.array(A, dtype=b.dtype)
-        y = xp.tensordot(A, b, axes=[[1], [-1]])
-        return xp.moveaxis(y, 0, -1)
+class Oblique(pyca.ProjOp):
+    # f: \bR^{N} -> \bR^{N}
+    #      x     -> (\alpha E_{N,1} + E_{N, N}) x
+    def __init__(self, N: int, alpha: float):
+        super().__init__(shape=(N, N))
+        self._alpha = float(alpha)
 
     @pycrt.enforce_precision("arr")
     def apply(self, arr):
-        return self._bcast_apply(self._A, arr)
+        out = np.zeros_like(arr)
+        out[..., -1] = (self._alpha * arr[..., 0]) + arr[..., -1]
+        return out
 
     @pycrt.enforce_precision("arr")
     def adjoint(self, arr):
-        return self._bcast_apply(self._A.T, arr)
+        out = np.zeros_like(arr)
+        out[..., 0] = self._alpha * arr[..., -1]
+        out[..., -1] = arr[..., -1]
+        return out
 
 
-class TestCabinetProjection(conftest.ProjOpT):
+class TestOblique(conftest.ProjOpT):
     @pytest.fixture
-    def angle(self):
-        return np.pi / 4
-
-    @pytest.fixture
-    def op(self, angle):
-        return CabinetProjection(angle)
-
-    @pytest.fixture
-    def data_shape(self):
-        return (3, 3)
+    def dim(self):
+        return 5
 
     @pytest.fixture
-    def data_apply(self, angle):
-        x = self._random_array((3,))
-        y = np.array(
-            [
-                x[0] + 0.5 * np.cos(angle) * x[2],
-                x[1] + 0.5 * np.sin(angle) * x[2],
-                0,
-            ]
-        )
+    def alpha(self):
+        return 3.1
+
+    @pytest.fixture
+    def op(self, dim, alpha):
+        return Oblique(N=dim, alpha=alpha)
+
+    @pytest.fixture
+    def data_shape(self, dim):
+        return (dim, dim)
+
+    @pytest.fixture
+    def data_apply(self, dim, alpha):
+        x = self._random_array((dim,))
+        y = np.zeros(dim)
+        y[-1] = alpha * x[0] + x[-1]
         return dict(
             in_=dict(arr=x),
             out=y,
