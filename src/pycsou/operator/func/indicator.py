aa0d6ddae6d937f2d70836c613de9bbcb2390893
Adrian Jarret <adrian.jarret@epfl.ch>
2022-10-21 10:45:02 +0200

    [PosConstraint] Implement a positivity constaint using indicator functon

--- src/pycsou/operator/func/indicator.py
+++ src/pycsou/operator/func/indicator.py
@@ -20,6 +20,7 @@ __all__ = [
     "RangeSet",
     "AffineSet",
     "ConvexSetIntersection",
+    "NonNegativeOrthant"
 ]
 
 
@@ -536,3 +537,31 @@ class ConvexSetIntersection(_IndicatorFunction):
                 y = f.prox(x - r[i], tau=1)  # tau is arbitrary
                 r[i] = y - x
                 x[:] = y
+
+
+class NonNegativeOrthant(pyca.ProxFunc):
+    """
+    Indicator function of the non-negative orthant (positivity constraint).
+
+    It is used to enforce non-negative real solutions and defined as:
+
+    .. math::
+
+       \iota(\mathbf{x}):=\begin{cases}
+        0 \,\text{if} \,\mathbf{x}\in \mathbb{R}^N_+,\\
+         \, +\infty\,\text{ortherwise}.
+         \end{cases}
+    """
+
+    def __init__(self, shape: pyct.Shape):
+        super().__init__(shape=shape)
+
+    @pycrt.enforce_precision(i="arr")
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.where(xp.all(arr >= 0, axis=-1), 0.0, np.infty)
+
+    @pycrt.enforce_precision(i="arr")
+    def prox(self, arr: pyct.NDArray, tau: pyct.Real) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        return xp.clip(arr, 0.0, None)
