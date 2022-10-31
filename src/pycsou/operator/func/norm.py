a5e0e539afa885d23cace87e878d83490f8f9043
Daniele Hamm <daniele.hamm@epfl.ch>
2022-10-31 19:09:40 +0100

    Implement Ball Norms

--- src/pycsou/operator/func/norm.py
+++ src/pycsou/operator/func/norm.py
@@ -221,3 +221,119 @@ class LInftyNorm(ShiftLossMixin, pyca.ProxFunc):
         y = xp.fmin(xp.fabs(arr), mu_star)
         y *= xp.sign(arr)
         return y
+
+
+class L1Ball(ShiftLossMixin, pyca.ProxFunc):
+    r"""
+    Indicator function of the :math:`\ell_1`-ball :math:`\{\mathbf{x}\in\mathbb{R}^N: \|\mathbf{x}\|_1\leq \text{radius}\}`
+
+    It is defined as:
+
+    .. math::
+
+       \iota(\mathbf{x}):=\begin{cases}
+        0 \,\text{if} \,\|\mathbf{x}\|_1\leq \text{radius},\\
+         \, +\infty\,\text{ortherwise}.
+         \end{cases}
+    """
+
+    def __init__(self, dim: pyct.Integer = None, radius: pyct.Real = 1):
+        super().__init__(shape=(1, dim))
+        self._radius = radius
+        self._lipschitz = np.inf
+
+    @pycrt.enforce_precision(i="arr")
+    @pycu.vectorize("arr")
+    def apply(self, arr: pyct.NDArray) -> pyct.Real:
+        xp = pycu.get_array_module(arr)
+        condition = pylinalg.norm(arr, ord=1) <= self._radius
+        return xp.zeros(1) if condition else np.inf * xp.ones(1)
+
+    @pycrt.enforce_precision(i=("arr", "tau"))
+    @pycu.vectorize("arr")
+    def prox(self, arr: pyct.NDArray, tau: pyct.Real) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        if pylinalg.norm(arr, ord=1) <= self._radius:
+            return arr
+        else:
+            mu_max = xp.max(xp.fabs(arr))
+            func = lambda mu: xp.sum(xp.fmax(xp.fabs(arr) - mu, 0)) - self._radius
+            mu_star = sciop.brentq(func, a=0, b=mu_max)
+            y = xp.fmax(0, xp.fabs(arr) - mu_star)
+            y *= xp.sign(arr)
+        return y
+
+
+class L2Ball(ShiftLossMixin, pyca.ProxFunc):
+    r"""
+    Indicator function of the :math:`\ell_2`-ball :math:`\{\mathbf{x}\in\mathbb{R}^N: \|\mathbf{x}\|_2\leq \text{radius}\}`
+
+    It is defined as:
+
+    .. math::
+
+       \iota(\mathbf{x}):=\begin{cases}
+        0 \,\text{if} \,\|\mathbf{x}\|_2\leq \text{radius},\\
+         \, +\infty\,\text{ortherwise}.
+         \end{cases}
+    """
+
+    def __init__(self, dim: pyct.Integer = None, radius: pyct.Real = 1):
+        super().__init__(shape=(1, dim))
+        self._radius = radius
+        self._lipschitz = np.inf
+
+    @pycrt.enforce_precision(i="arr")
+    @pycu.vectorize("arr")
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        condition = xp.linalg.norm(arr) <= self._radius
+        return xp.zeros(1) if condition else np.inf * xp.ones(1)
+
+    @pycrt.enforce_precision(i=("arr", "tau"))
+    @pycu.vectorize("arr")
+    def prox(self, arr: pyct.NDArray, tau: pyct.Real) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        arr_norm = xp.linalg.norm(arr)
+        if arr_norm <= self._radius:
+            return arr
+        else:
+            return self._radius * arr / arr_norm
+
+
+class LInftyBall(ShiftLossMixin, pyca.ProxFunc):
+    r"""
+    Indicator function of the :math:`\ell_\infty`-ball :math:`\{\mathbf{x}\in\mathbb{R}^N: \|\mathbf{x}\|_\infty\leq \text{radius}\}`
+
+    It is defined as:
+
+    .. math::
+
+       \iota(\mathbf{x}):=\begin{cases}
+        0 \,\text{if} \,\|\mathbf{x}\|_\infty\leq \text{radius},\\
+         \, +\infty\,\text{ortherwise}.
+         \end{cases}
+    """
+
+    def __init__(self, dim: pyct.Integer = None, radius: pyct.Real = 1):
+        super().__init__(shape=(1, dim))
+        self._radius = radius
+        self._lipschitz = np.inf
+
+    @pycrt.enforce_precision(i="arr")
+    @pycu.vectorize("arr")
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        condition = xp.max(xp.fabs(arr)) <= self._radius
+        return xp.zeros(1) if condition else np.inf * xp.ones(1)
+
+    @pycrt.enforce_precision(i=("arr", "tau"))
+    @pycu.vectorize("arr")
+    def prox(self, arr: pyct.NDArray, tau: pyct.Real) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        if xp.max(xp.fabs(arr)) <= self._radius:
+            return arr
+        else:
+            y = xp.fmin(xp.fabs(arr), self._radius)
+            y *= xp.sign(arr)
+            return y
