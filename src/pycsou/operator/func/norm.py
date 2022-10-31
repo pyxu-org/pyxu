8632840c3e4c37c83f1868fff42be7e6206e5166
Daniele Hamm <daniele.hamm@epfl.ch>
2022-10-31 15:47:34 +0100

    Implement L^2_1 norm

--- src/pycsou/operator/func/norm.py
+++ src/pycsou/operator/func/norm.py
@@ -1,16 +1,16 @@
+import cupy as cp
+import dask.array as ds
 import numpy as np
+import scipy.optimize as sciop
 
 import pycsou.abc as pyca
 import pycsou.math.linalg as pylinalg
 import pycsou.runtime as pycrt
 import pycsou.util as pycu
+import pycsou.util.array_module as pycam
 import pycsou.util.ptype as pyct
 
-__all__ = [
-    "L1Norm",
-    "L2Norm",
-    "SquaredL2Norm",
-]
+__all__ = ["L1Norm", "L2Norm", "SquaredL2Norm", "SquaredL1Norm", "LInftyNorm", "L1Ball", "L2Ball", "LInftyBall"]
 
 
 class ShiftLossMixin:
@@ -126,3 +126,68 @@ class SquaredL2Norm(pyca._QuadraticFunc):
             )
             raise ValueError(msg)
         return IdentityOp(dim=self.dim).squeeze()
+
+
+class SquaredL1Norm(ShiftLossMixin, pyca.ProxFunc):
+    r"""
+    :math:`\ell^2_1`-norm, :math:`\Vert\mathbf{x}\Vert^2_1:=(\sum_{i=1}^N |x_i|)^2`.
+    """
+
+    def __init__(self, dim: pyct.Integer = None, prox_computation: str = "sort"):
+        r"""
+        Parameters
+        ----------
+        dim: pyct.Integer
+            Dimension size. (Default: domain-agnostic.)
+        prox_computation: str
+            Algorithm for computing the proximal operator: 'root' uses [FirstOrd]_ Lemma 6.70, while 'sort' uses [OnKerLearn]_ Algorithm 2 (faster). (Default : 'sort'.)
+
+        Notes
+        -----
+        The operator's Lipschitz constant is set to :math:`\infty` if domain-agnostic.
+        It is recommended to set `dim` explicitly to compute a tight closed-form.
+        If module of input array is dask, algorithm 'root' is used independently of the user choice (sorting is not recommended in dask).
+        """
+        super().__init__(shape=(1, dim))
+        self._lipschitz = np.inf
+        self.prox_computation = prox_computation
+
+    @pycrt.enforce_precision(i="arr")
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        y = pylinalg.norm(arr, ord=1, axis=-1, keepdims=True)
+        y **= 2
+        return y
+
+    @pycrt.enforce_precision(i=("arr", "tau"))
+    def _prox_root(self, arr: pyct.NDArray, tau: pyct.Real) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        if xp.linalg.norm(arr) > 0:
+            mu_max = xp.max(xp.fabs(arr) ** 2) / (4 * tau)
+            mu_min = 1e-12
+            func = lambda mu: xp.sum(xp.fmax(xp.fabs(arr) * xp.sqrt(tau / mu) - 2 * tau, 0)) - 1
+            mu_star = sciop.brentq(func, a=mu_min, b=mu_max)
+            lambda_ = xp.fmax(xp.abs(arr) * xp.sqrt(tau / mu_star) - 2 * tau, 0)
+            return lambda_ * arr / (lambda_ + 2 * tau)
+        else:
+            return arr
+
+    @pycrt.enforce_precision(i=("arr", "tau"))
+    @pycam.redirect("arr", DASK=_prox_root)
+    def _prox_sort(self, arr: pyct.NDArray, tau: pyct.Real) -> pyct.NDArray:
+        xp = pycu.get_array_module(arr)
+        z = xp.sort(xp.abs(arr))[::-1]
+        cumsum_z = xp.cumsum(z)
+        test_array = z - (2 * tau / (1 + (xp.arange(z.size) + 1) * 2 * tau)) * cumsum_z
+        max_nzi = xp.max(xp.nonzero(test_array.reshape(-1) > 0)[0])
+        threshold = (2 * tau / (1 + (max_nzi + 1) * 2 * tau)) * cumsum_z[max_nzi]
+        y = xp.fmax(0, xp.fabs(arr) - threshold)
+        y *= xp.sign(arr)
+        return y
+
+    @pycu.vectorize("arr")
+    def prox(self, arr: pyct.NDArray, tau: pyct.Real) -> pyct.NDArray:
+        arr = arr.flatten()
+        if self.prox_computation == "root":
+            return self._prox_root(arr, tau)
+        elif self.prox_computation == "sort":
+            return self._prox_sort(arr, tau)
