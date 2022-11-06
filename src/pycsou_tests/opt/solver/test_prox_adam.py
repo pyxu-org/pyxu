00d3b1bb2eb44f5289e3441c39aa93257da2c52b
Dicedead <dicedead@gmail.com>
2022-11-06 12:30:25 +0100

    [test][prox_adam] test suite for prox_adam

---
+++ src/pycsou_tests/opt/solver/test_prox_adam.py
@@ -0,0 +1,45 @@
+import itertools
+
+import numpy as np
+import pytest
+
+import pycsou.opt.solver as pycos
+import pycsou.util.ptype as pyct
+import pycsou_tests.opt.solver.conftest as conftest
+from pycsou.operator import shift_loss
+
+
+class TestProxAdam(conftest.SolverT):
+    @staticmethod
+    def spec_data(N: int) -> list[tuple[pyct.SolverC, dict, dict]]:
+        from pycsou.operator.func import L1Norm, SquaredL2Norm
+
+        klass = [
+            pycos.ProxAdam,
+        ]
+        kwargs_init = [
+            dict(f=shift_loss(SquaredL2Norm(dim=N), np.full((N,), 3.0)), g=L1Norm(dim=N)),
+        ]
+
+        kwargs_fit = []
+        param_sweep = dict(
+            x0=[
+                np.full((N,), 0.0),
+                np.full((1, N), 0.0),
+                np.full((3, 4, 2, N), 0.0),
+                np.full((2, N), 0.0),  # multiple initial points
+            ],
+            variant=["adam", "amsgrad", "padam"],
+            p=[0.25],
+        )
+        for config in itertools.product(*param_sweep.values()):
+            d = dict(zip(param_sweep.keys(), config))
+            kwargs_fit.append(d)
+
+        data = itertools.product(klass, kwargs_init, kwargs_fit)
+        return list(data)
+
+    @pytest.fixture(params=spec_data(N=7))
+    def spec(self, request) -> tuple[pyct.SolverC, dict, dict]:
+        klass, kwargs_init, kwargs_fit = request.param
+        return klass, kwargs_init, kwargs_fit
