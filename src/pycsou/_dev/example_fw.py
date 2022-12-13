ecdc656aa511a4cb781b76b4ae3ced00e066b073
Adrian Jarret <adrian.jarret@epfl.ch>
2022-12-13 14:36:25 +0100

    Try some post processing in the example.

--- src/pycsou/_dev/example_fw.py
+++ src/pycsou/_dev/example_fw.py
@@ -7,16 +7,14 @@ import numpy as np
 
 import pycsou._dev.fw_utils as pycdevu
 import pycsou.abc as pyca
-import pycsou.operator
 import pycsou.operator as pycop
 import pycsou.opt.solver.fw_lasso as pycfw
 import pycsou.opt.stop as pycos
-from pycsou.abc.operator import LinOp
 from pycsou.opt.solver.pgd import PGD
 
 matplotlib.use("Qt5Agg")
 
-seed = 496  # for reproducibility
+seed = None  # for reproducibility
 
 # Dimensions of the problem
 L = 1000
@@ -26,16 +24,18 @@ psnr = 20
 
 # Parameters for reconstruction
 lambda_factor = 0.1
+
+# Parameters of the solver
 remove = True
 eps = 1e-4
 min_iterations = 1
 tmax = 15.0
-ms_threshold = 0.9
+ms_threshold = 0.8
 init_correction = 1e-1
 final_correction = 1e-6
+eps_dcv = 1e-2
 
-# alternative stopping criteria
-dcv = pycfw.dcvStoppingCrit(1e-3)
+
 stop_crit = pycos.RelError(
     eps=eps,
     var="objective_func",
@@ -43,7 +43,8 @@ stop_crit = pycos.RelError(
     norm=2,
     satisfy_all=True,
 )
-
+# alternative stopping criteria
+dcv = pycfw.dcvStoppingCrit(eps_dcv)
 # Minimum number of iterations
 min_iter = pycos.MaxIter(n=min_iterations)
 
@@ -59,7 +60,7 @@ if __name__ == "__main__":
 
     mat = rng.normal(size=(L, N))  # forward matrix
     indices = rng.choice(N, size=k)  # indices of active components in the source
-    injection = pycop.SubSample(N, indices).T  # pycdevu.SubSampling(size=N, sampling_indices=indices).T
+    injection = pycop.SubSample(N, indices).T
     source = injection(rng.normal(size=k))  # sparse source
 
     op = pyca.LinOp.from_array(mat)
@@ -124,8 +125,10 @@ if __name__ == "__main__":
     plt.legend()
     plt.subplot(122)
     plt.plot(pfw._mstate["correction_iterations"], label="iterations")
+    plt.ylim(bottom=0.0)
     plt.twinx()
     plt.plot(pfw._mstate["correction_durations"], label="duration", c="r")
+    plt.ylim(bottom=0.0)
     plt.title("Correction iterations")
     plt.legend()
     plt.show()
@@ -137,12 +140,12 @@ if __name__ == "__main__":
         )
     )
 
-    # Solving the same problems with another stopping criterion: DCV
-    vfw.fit(stop_crit=(min_iter & dcv) | pycos.MaxDuration(t=dt.timedelta(seconds=tmax)))
-    data_v_dcv, hist_v_dcv = vfw.stats()
-
-    pfw.fit(stop_crit=(min_iter & dcv) | pycos.MaxDuration(t=dt.timedelta(seconds=tmax)))
-    data_p_dcv, hist_p_dcv = pfw.stats()
+    # # Solving the same problems with another stopping criterion: DCV
+    # vfw.fit(stop_crit=(min_iter & dcv) | pycos.MaxDuration(t=dt.timedelta(seconds=tmax)))
+    # data_v_dcv, hist_v_dcv = vfw.stats()
+    #
+    # pfw.fit(stop_crit=(min_iter & dcv) | pycos.MaxDuration(t=dt.timedelta(seconds=tmax)))
+    # data_p_dcv, hist_p_dcv = pfw.stats()
 
     plt.figure(figsize=(10, 8))
     plt.suptitle("Compare the reconstructions")
@@ -156,47 +159,66 @@ if __name__ == "__main__":
     plt.subplot(212)
     plt.stem(source, label="source", linefmt="C0-", markerfmt="C0o")
     plt.stem(data_apgd["x"], label="apgd", linefmt="C1:", markerfmt="C1x")
-    plt.stem(data_v_dcv["x"], label="VFW", linefmt="C2:", markerfmt="C2x")
-    plt.stem(data_p_dcv["x"], label="PFW", linefmt="C3:", markerfmt="C3x")
-    plt.legend()
+    # plt.stem(data_v_dcv["x"], label="VFW", linefmt="C2:", markerfmt="C2x")
+    # plt.stem(data_p_dcv["x"], label="PFW", linefmt="C3:", markerfmt="C3x")
+    # plt.legend()
     plt.title("Dual certificate value as stopping criterion")
     plt.show()
 
+    mini = min(
+        hist_p["Memorize[objective_func]"][-1],
+        hist_v["Memorize[objective_func]"][-1],
+        hist_apgd["Memorize[objective_func]"][-1],
+    )
     plt.figure(figsize=(10, 8))
     plt.suptitle("Objective function values")
     plt.subplot(211)
     plt.yscale("log")
-    plt.plot(hist_p["duration"], hist_p["Memorize[objective_func]"], label="PFW")
-    plt.plot(hist_v["duration"], hist_v["Memorize[objective_func]"], label="VFW")
-    plt.plot(hist_apgd["duration"], hist_apgd["Memorize[objective_func]"], label="APGD")
+    plt.plot(
+        hist_p["duration"],
+        (hist_p["Memorize[objective_func]"] - mini) / (hist_apgd["Memorize[objective_func]"][0]),
+        label="PFW",
+        marker="+",
+    )
+    plt.plot(
+        hist_v["duration"],
+        (hist_v["Memorize[objective_func]"] - mini) / (hist_apgd["Memorize[objective_func]"][0]),
+        label="VFW",
+    )
+    plt.plot(
+        hist_apgd["duration"],
+        (hist_apgd["Memorize[objective_func]"] - mini) / (hist_apgd["Memorize[objective_func]"][0]),
+        label="APGD",
+        marker="x",
+    )
     plt.legend()
     plt.ylabel("OFV")
     plt.title("Stop: Relative improvement ofv")
 
     plt.subplot(212)
     plt.yscale("log")
-    plt.plot(hist_p_dcv["duration"], hist_p_dcv["Memorize[objective_func]"], label="PFW")
-    plt.plot(hist_v_dcv["duration"], hist_v_dcv["Memorize[objective_func]"], label="VFW")
-    plt.plot(hist_apgd["duration"], hist_apgd["Memorize[objective_func]"], label="APGD")
-    plt.ylabel("OFV")
-    plt.legend()
-    plt.title("Stop: Absolute error dcv")
+    # plt.plot(hist_p_dcv["duration"], hist_p_dcv["Memorize[objective_func]"], label="PFW")
+    # plt.plot(hist_v_dcv["duration"], hist_v_dcv["Memorize[objective_func]"], label="VFW")
+    # plt.plot(hist_apgd["duration"], hist_apgd["Memorize[objective_func]"], label="APGD")
+    # plt.ylabel("OFV")
+    # plt.legend()
+    # plt.title("Stop: Absolute error dcv")
     plt.show()
 
     plt.figure(figsize=(10, 8))
     plt.suptitle("Stopping criterion values")  # TO CHAAAAAAAAAAANGE + time x-axis + push
     plt.subplot(211)
     plt.yscale("log")
-    plt.plot(hist_p["duration"], hist_p["RelError[objective_func]"], label="PFW")
+    plt.plot(hist_p["duration"], hist_p["RelError[objective_func]"], label="PFW", marker="+")
     plt.plot(hist_v["duration"], hist_v["RelError[objective_func]"], label="VFW")
     plt.title("Stop: Relative improvement ofv")
     plt.legend()
     plt.subplot(212)
     plt.yscale("log")
-    plt.plot(hist_p_dcv["duration"], hist_p_dcv["AbsError[dcv]"], label="PFW")
-    plt.plot(hist_v_dcv["duration"], hist_v_dcv["AbsError[dcv]"], label="VFW")
-    plt.title("Stop: Absolute error dcv")
-    plt.legend()
+    # plt.plot(hist_p_dcv["duration"], hist_p_dcv["AbsError[dcv]"], label="PFW")
+    # plt.plot(hist_v_dcv["duration"], hist_v_dcv["AbsError[dcv]"], label="VFW")
+    # plt.title("Stop: Absolute error dcv")
+    # plt.legend()
     plt.show()
 
     # print("Solving with APGD and specified support: ...")
@@ -219,5 +241,10 @@ if __name__ == "__main__":
     # plt.stem(ss.T(data_rsapgd["x"]), label="rs apgd", linefmt="C2:", markerfmt="C2x")
     # plt.legend()
     # plt.show()
+
+    start = time.time()
+    pfw.post_process()
+    print(f"Post-processing in: {time.time()-start:.4f}s")
+    print(f"Final value of the objective: {pfw.objective_func()[0]:.4f}")
 
 # todo change the reweighting with specified number of iterations: 1/2/5/10 ? More iterations for the last one ?
