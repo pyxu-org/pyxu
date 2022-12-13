48de58c0d0b47d38ed7249660366d7d25e11b472
Sepand KASHANI <kashani.sepand@gmail.com>
2022-12-13 09:15:30 +0100

    [prox_adam] Overhaul docstrings.

--- src/pycsou/opt/solver/prox_adam.py
+++ src/pycsou/opt/solver/prox_adam.py
@@ -13,7 +13,7 @@ __all__ = [
 
 class ProxAdam(pyca.Solver):
     r"""
-    Proximal Adam solver.
+    Proximal Adam solver [ProxAdam]_.
 
     ProxAdam minimizes
 
@@ -28,21 +28,32 @@ class ProxAdam(pyca.Solver):
     * :math:`\mathcal{G}:\mathbb{R}^N\rightarrow \mathbb{R}\cup\{+\infty\}` is a *proper*, *lower
       semicontinuous* and *convex function* with a *simple proximal operator*.
 
-    It does so by estimating the mean and the variance of :math:`\mathbf{g}_t=\nabla\mathcal{F}` at each
-    iteration by :math:`\phi_t=\phi(\mathbf{g}_1, \ldots, \mathbf{g}_t)` and
-    :math:`\psi_t=\psi(\mathbf{g}_1^2, \ldots, \mathbf{g}_t^2)` respectively first, then solving the proximal
-    sub-problem:
+    ProxAdam is a suitable alternative to Proximal Gradient Descent
+    (:py:class:`~pycsou.opt.solvel.pgd.PGD`) when:
 
-    ..math::
+    * computing :math:`\beta` to optimally choose the step size is infeasible, and
+    * line-search methods to estimate step sizes are too expensive.
 
-       {\min_{\mathbf{z}\in\mathbb{R}^N} \;\mathcal{G}(\mathbf{z})\;\;+\;\;\frac{1}{2\alpha}||\mathbf{z}-\mathbf{x}_t||_H^2},
+    Compared to PGD, ProxAdam:
 
-    where :math:`H=\text{diag}(\psi_t)`, which can be solved through Proximal Gradient Descent.
-    Three variants of this algorithm are implemented, corresponding to three distinct :math:`\phi` and :math:`psi` pairs:
+    * auto-tunes gradient updates based on stochastic estimates of
+      :math:`\phi_{t} = \mathbb{E}[\nabla\mathcal{F}]` and :math:`\psi_{t} =
+      \mathbb{E}[\nabla\mathcal{F}^{2}]` respectively;
+    * uses a modified proximity operator at each iteration to update coordinates at varying scales:
+
+    .. math::
+
+       \text{prox}_{\alpha\mathcal{G}}(\mathbf{x}_{t})
+       =
+       \min_{\mathbf{z}\in\mathbb{R}^N} \;\mathcal{G}(\mathbf{z})\;\;+\;\;\frac{1}{2\alpha}||\mathbf{z}-\mathbf{x}_t||_H^2,
+
+    where :math:`H=\text{diag}(\psi_t)`.
+    The modified proximity operator is computed by solving a PGD sub-problem.
+    ProxAdam has many named variants for particular choices of :math:`\phi` and :math:`\psi`:
 
     * Adam:
 
-    ..math::
+    .. math::
 
        \phi_t
        =
@@ -50,7 +61,8 @@ class ProxAdam(pyca.Solver):
          \mathbf{m}_t
        }{
          1-\beta_1^t
-       } \\
+       }
+       \qquad
        \psi_t
        =
        \sqrt{
@@ -63,21 +75,23 @@ class ProxAdam(pyca.Solver):
 
     * AMSGrad:
 
-    ..math::
+    .. math::
 
-       \phi_t = \mathbf{m}_t \\
+       \phi_t = \mathbf{m}_t
+       \qquad
        \psi_t = \sqrt{\hat{\mathbf{v}}_t}
 
     * PAdam:
 
-    ..math::
+    .. math::
 
-       \phi_t = \mathbf{m}_t \\
-       \psi_t = \hat{\mathbf{v}}_t^p
+       \phi_t = \mathbf{m}_t
+       \qquad
+       \psi_t = \hat{\mathbf{v}}_t^p,
 
     where in all cases:
 
-    ..math::
+    .. math::
 
        \mathbf{m}_t
        =
@@ -91,22 +105,20 @@ class ProxAdam(pyca.Solver):
        (1-\beta_2)\mathbf{g}_t^2\\
        \hat{\mathbf{v}}_t
        =
-       \max(\hat{\mathbf{v}}_{t-1}, \mathbf{v}_t)
+       \max(\hat{\mathbf{v}}_{t-1}, \mathbf{v}_t),
 
     with :math:`\mathbf{m}_0 = \mathbf{v}_0 = \mathbf{0}`.
 
-    Further details in [ProxAdam]_.
-
     **Remark 1:**
-    The algorithm is still valid if :math:`\mathcal{G}` is zero. It is not if :math:`\mathcal{F}` is zero.
+    The algorithm is still valid if :math:`\mathcal{G}` is zero.
 
     **Remark 2:**
     The convergence is guaranteed for step sizes :math:`\alpha\leq 2/\beta`.
 
     **Remark 3:**
     The relative norm change of the primal variable is used as the default stopping criterion.
-    By default, the algorithm stops when the norm of the difference between two consecutive
-    iterates :math:`\{\mathbf{x}_n\}_{n\in\mathbb{N}}` is smaller than 1e-4.
+    By default, the algorithm stops when the norm of the difference between two consecutive iterates
+    :math:`\{\mathbf{x}_n\}_{n\in\mathbb{N}}` is smaller than 1e-4.
     Different stopping criteria can be used. (see :py:mod:`~pycsou.opt.solver.stop`.)
     By default, the same stopping criterion is used for the proximal sub-problem.
 
@@ -114,56 +126,61 @@ class ProxAdam(pyca.Solver):
 
     x0: pyct.NDArray
         (..., N) initial point(s).
-    variant: str
+    variant: "adam", "amsgrad", "padam"
         Name of the ProxAdam variant to use.
-        Use "adam" for Adam. (default)
-        Use "amsgrad" for AMSGrad.
-        Use "padam" for PAdam.
+        Defaults to "adam"
     a: pyct.Real
-        Gradient step size.
+        Max normalized gradient step size.
         Defaults to :math:`1 / \beta` if unspecified.
     b1: pyct.Real
-        Factor for mean gradient computation.
+        1st-order gradient exponential decay :math:`\beta_{1} \in [0, 1)`.
     b2: pyct.Real
-        Factor for gradient variance computation.
+        2nd-order gradient exponential decay :math:`\beta_{2} \in [0, 1)`.
     m0: pyct.NDArray
-        (..., N) first mean estimate of the gradient corresponding to
-        each initial point, defaults to the zero vector.
+        (..., N) initial 1st-order gradient estimate corresponding to each initial point.
+        Defaults to the null vector if unspecified.
     v0: pyct.NDArray
-        (..., N) first variance estimate of the gradient corresponding to
-        each initial point, defaults to the zero vector.
+        (..., N) initial 2nd-order gradient estimate corresponding to each initial point.
+        Defaults to the null vector if unspecified.
     stop_crit_sub: pyca.solver.StoppingCriterion
-        Sub-problem stopping criterion, defaults to the main problem's.
+        Sub-problem stopping criterion.
+        Default: use same stopping criterion as main problem.
     **kwargs
         p: pyct.Real
-            Power used in the variance estimate. Must be specified for PAdam, unused else. Should be between 0 and 0.5.
+            PAdam power parameter :math:`p \in [0, 0.5]`.
+            Must be specified for PAdam, unused otherwise.
         eps_adam: pyct.Real
-            Noise term used exclusively in Adam, unused else. Defaults to 1e-6.
+            Adam noise parameter :math:`\epsilon`.
+            This term is used exclusively if `variant="adam"`.
+            Defaults to 1e-6.
 
     Example
     --------
-    Consider the following optimisation problem:
+    Consider the following optimization problem:
 
-    ..math:
+    .. math::
 
        \min_{\mathbf{x}\in\mathbb{R}^N} \Vert{\mathbf{x}-\mathbf{1}}\Vert_2^2 + \Vert{\mathbf{x}-\mathbf{1}}\Vert_1
-    
-    >>> import numpy as np
 
-    >>> from pycsou.operator import shift_loss
-    >>> from pycsou.operator.func import L2Norm, SquaredL2Norm
-    >>> from pycsou.opt.solver import ProxAdam
+    .. code-block:: python3
 
-    >>> N = 3
-    >>> f = SquaredL2Norm(dim=N).asloss(np.ones((N,)))
-    >>> g = L2Norm(dim=N).asloss(np.ones((N,)))
+       import numpy as np
 
-    >>> prox_adam = ProxAdam(f, g)
-    >>> x0 = np.zeros((N,))
-    >>> prox_adam.fit(x0=x0, variant="padam", p=0.25)
-    >>> x_star = prox_adam.solution()
-    >>> assert np.allclose(x_star, np.ones((N,)))
+       from pycsou.operator.func import L1Norm, SquaredL2Norm
+       from pycsou.opt.solver import ProxAdam
 
+       N = 3
+       f = SquaredL2Norm(dim=N).asloss(1)
+       g = L1Norm(dim=N).asloss(1)
+
+       prox_adam = ProxAdam(f, g)
+       prox_adam.fit(
+           x0=np.zeros((N,)),
+           variant="padam",
+           p=0.25,
+       )
+       x_opt = prox_adam.solution()
+       np.allclose(x_opt, 1)  # True
     """
     __eps_adam_default = 1e-6
 
