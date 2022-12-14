ad3c6989a759bec9c7878a8bdba415bb0aaafd8d
MatthieuMeo <matthieu.simeoni@gmail.com>
2022-12-14 16:29:10 +0100

    [dev][NUFFT3] Script for performing a manual NUFFT3 (NUFFT1->iFFT->NUFFT2 = NUFFT3).
    Cannot seem to get below an error of 5%, no matter what I choose for eps... To be investigated.

--- src/pycsou/_dev/nufft/manual_nufft3.py
+++ src/pycsou/_dev/nufft/manual_nufft3.py
@@ -8,50 +8,50 @@ import pycsou.util as pycu
 
 nufft_kwargs = dict(
     real=False,
-    eps=1e-9,
     isign=-1,
     n_trans=5,
     nthreads=0,
     modeord=1,
-    upsampfac=2,
 )
 
+eps = 1e-5
 rng = np.random.default_rng(1)
 D, M, N = 2, 200, 50
 x = rng.normal(size=(M, D))
 z = rng.normal(size=(N, D))
 
-A = nufft.NUFFT.type3(x, z, **nufft_kwargs)
-n_modes = np.ceil(np.array(A.fft_size[0])).astype(int).tolist()
-h = 2 * np.pi / np.array(n_modes)
-gamma = A._dilationfac
-A1 = nufft.NUFFT.type1(x / gamma, n_modes, **nufft_kwargs)
-A2 = nufft.NUFFT.type2(z * gamma * h, n_modes, **nufft_kwargs)
+xp = pycu.get_array_module(x)
+
+A = nufft.NUFFT.type3(x, z, eps=eps, **nufft_kwargs)
+A0 = nufft.NUFFT.type3(x, z, eps=0, **nufft_kwargs)
+
+n_modes = A._fft_shape()
+X, x0 = A._shift_coords(x)
+Z, z0 = A._shift_coords(z)
+gamma = xp.array(A._dilation_factor(), dtype=x.dtype)
+A1 = nufft.NUFFT.type1(x / gamma, n_modes, eps=eps / 2, **nufft_kwargs)
+A2 = nufft.NUFFT.type2(z * gamma * 2 * np.pi / xp.array(n_modes, dtype=x.dtype), n_modes, eps=eps / 2, **nufft_kwargs)
 
 arr = rng.normal(size=(34, nufft_kwargs["n_trans"], M)).reshape(-1, M)
 if not nufft_kwargs["real"]:
     arr = arr + 1j * rng.normal(size=arr.shape)
 
-y0 = pycu.view_as_real(A._nudft_apply(arr))
+y0 = A0.apply(pycu.view_as_real(arr))
 y_ref = A.apply(pycu.view_as_real(arr))
 y_man = pycu.view_as_complex(A1.apply(pycu.view_as_real(arr)))
 y_man = fft.ifftn(
-    y_man.reshape(
-        [
-            -1,
-        ]
-        + n_modes
-    ),
+    y_man.reshape((-1,) + n_modes),
     axes=(-2, -1),
     norm="backward",
     workers=-1,
 )
-y_man = A2.apply(pycu.view_as_real(y_man).reshape(-1, A2.dim))
+y_man = A2.apply(pycu.view_as_real(y_man.reshape(-1, np.prod(n_modes))))
 
 print(np.linalg.norm(y_man - y_ref) / np.linalg.norm(y_ref))
 print(np.linalg.norm(y_man - y0) / np.linalg.norm(y0))
 print(np.linalg.norm(y0 - y_ref) / np.linalg.norm(y0))
 
-plt.plot(y0[0])
-plt.plot(y_ref[0])
-plt.plot(y_man[0])
+plt.plot(y0[1])
+plt.plot(y_ref[1])
+plt.plot(y_man[1])
+plt.show()
