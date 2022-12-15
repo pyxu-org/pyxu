04cf66fa63e892eabac6a967b332f0d7b7dd2b6f
MatthieuMeo <matthieu.simeoni@gmail.com>
2022-12-15 16:23:52 +0100

    [dev][NUFFT] Scripts for FFT via NUFFT and NUFFT3 via NUFFT1>iFFT>NUFFT2 fixed.

---
+++ src/pycsou/_dev/nufft/fft_via_nufft.py
@@ -0,0 +1,56 @@
+import time as t
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.fft as fft
+import scipy.linalg as sclin
+
+import pycsou.operator.linop.nufft as nufft
+import pycsou.runtime as pycrt
+import pycsou.util as pycu
+
+nufft_kwargs = dict(
+    real=False,
+    isign=-1,
+    nthreads=0,
+    modeord=1,
+    upsampfac=2,
+)
+
+
+eps = 1e-5
+rng = np.random.default_rng(1)
+D, N = 2, 25
+freqs = fft.fftfreq(N) * 2 * np.pi
+
+A = nufft.NUFFT.type1(freqs, N, eps=eps, **nufft_kwargs)
+
+plt.figure()
+plt.subplot(1, 2, 1)
+plt.imshow(A.asarray())
+plt.colorbar()
+plt.subplot(1, 2, 2)
+plt.imshow(pycu.view_as_real_mat(sclin.dft(N)))
+plt.colorbar()
+
+
+arr = rng.normal(size=N)
+if not nufft_kwargs["real"]:
+    arr = arr + 1j * rng.normal(size=arr.shape)
+
+tic = t.time()
+y_ref = fft.fft(
+    arr,
+    norm="backward",
+    workers=-1,
+)
+print(f"FFT: {t.time() - tic} secs")
+tic = t.time()
+y_man = pycu.view_as_complex(A.apply(pycu.view_as_real(arr)))
+print(f"NUFFT: {t.time() - tic} secs")
+print(f"Acurracy: {np.linalg.norm(y_man - y_ref) / np.linalg.norm(y_ref)}")
+
+plt.figure()
+plt.plot(pycu.view_as_real(y_ref))
+plt.plot(pycu.view_as_real(y_man))
+plt.show()
--- src/pycsou/_dev/nufft/manual_nufft3.py
+++ src/pycsou/_dev/nufft/manual_nufft3.py
@@ -9,16 +9,16 @@ import pycsou.util as pycu
 nufft_kwargs = dict(
     real=False,
     isign=-1,
-    n_trans=5,
     nthreads=0,
     modeord=1,
+    upsampfac=1.25,
 )
 
-eps = 1e-5
+eps = 1e-7
 rng = np.random.default_rng(1)
-D, M, N = 2, 200, 50
-x = rng.normal(size=(M, D))
-z = rng.normal(size=(N, D))
+D, M, N = 1, 150, 250
+x = rng.uniform(-1, 1, size=(M, D))
+z = rng.uniform(-1, 1, size=(N, D))
 
 xp = pycu.get_array_module(x)
 
@@ -30,28 +30,57 @@ X, x0 = A._shift_coords(x)
 Z, z0 = A._shift_coords(z)
 gamma = xp.array(A._dilation_factor(), dtype=x.dtype)
 A1 = nufft.NUFFT.type1(x / gamma, n_modes, eps=eps / 2, **nufft_kwargs)
+mesh1 = A1.mesh(scale="source", upsampled=True)
 A2 = nufft.NUFFT.type2(z * gamma * 2 * np.pi / xp.array(n_modes, dtype=x.dtype), n_modes, eps=eps / 2, **nufft_kwargs)
 
-arr = rng.normal(size=(34, nufft_kwargs["n_trans"], M)).reshape(-1, M)
+arr = rng.normal(10, size=M)
 if not nufft_kwargs["real"]:
-    arr = arr + 1j * rng.normal(size=arr.shape)
+    arr = arr + 1j * rng.normal(10, size=arr.shape)
 
 y0 = A0.apply(pycu.view_as_real(arr))
 y_ref = A.apply(pycu.view_as_real(arr))
 y_man = pycu.view_as_complex(A1.apply(pycu.view_as_real(arr)))
-y_man = fft.ifftn(
-    y_man.reshape((-1,) + n_modes),
-    axes=(-2, -1),
+y_man = fft.ifft(
+    y_man,
     norm="backward",
     workers=-1,
 )
-y_man = A2.apply(pycu.view_as_real(y_man.reshape(-1, np.prod(n_modes))))
+y_man = A2.apply(pycu.view_as_real(y_man))
 
 print(np.linalg.norm(y_man - y_ref) / np.linalg.norm(y_ref))
-print(np.linalg.norm(y_man - y0) / np.linalg.norm(y0))
-print(np.linalg.norm(y0 - y_ref) / np.linalg.norm(y0))
+# print(np.linalg.norm(y_man - y0) / np.linalg.norm(y0))
+# print(np.linalg.norm(y0 - y_ref) / np.linalg.norm(y0))
 
-plt.plot(y0[1])
-plt.plot(y_ref[1])
-plt.plot(y_man[1])
+plt.figure()
+# plt.plot(y0)
+plt.plot(y_ref)
+plt.plot(y_man)
+plt.show()
+
+z_ref = A.adjoint(y_ref)
+z_man = pycu.view_as_complex(A2.adjoint(y_ref))
+z_man = fft.fft(
+    z_man,
+    norm="forward",
+    workers=-1,
+)
+z_man = A1.adjoint(pycu.view_as_real(z_man))
+print(np.linalg.norm(z_man - z_ref) / np.linalg.norm(z_ref))
+
+plt.figure()
+plt.plot(z_ref)
+plt.plot(z_man)
+plt.show()
+
+plt.figure()
+plt.subplot(2, 1, 1)
+# plt.stem(x.squeeze() / gamma, np.abs(arr))
+plt.scatter(
+    x.squeeze() / gamma,
+    np.abs(pycu.view_as_complex(z_ref)),
+)
+plt.scatter(x.squeeze() / gamma, np.abs(pycu.view_as_complex(z_man)))
+plt.subplot(2, 1, 2)
+plt.scatter(z.squeeze() * gamma * 2 * np.pi / xp.array(n_modes, dtype=x.dtype), np.abs(pycu.view_as_complex(y_ref)))
+plt.scatter(z.squeeze() * gamma * 2 * np.pi / xp.array(n_modes, dtype=x.dtype), np.abs(pycu.view_as_complex(y_man)))
 plt.show()
