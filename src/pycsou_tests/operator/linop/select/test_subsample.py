20c57c77e0985ff210ac24e07f06337aa519ceb7
Sepand KASHANI <kashani.sepand@gmail.com>
2022-12-15 18:47:53 +0100

    [test_trim] Refactor common SubSample/Trim() test code to parent Mixin.

--- src/pycsou_tests/operator/linop/select/test_subsample.py
+++ src/pycsou_tests/operator/linop/select/test_subsample.py
@@ -9,7 +9,35 @@ import pycsou.util.deps as pycd
 import pycsou_tests.operator.conftest as conftest
 
 
-class TestSubSample(conftest.LinOpT):
+class SubSampleMixin:
+    @pytest.fixture
+    def arg_shape(self, request):  # anything which is understood as shape parameter of np.zeros()
+        raise NotImplementedError
+
+    @pytest.fixture
+    def indices(self, request):  # anything which can index an NDArray
+        raise NotImplementedError
+
+    @pytest.fixture
+    def sub_shape(self, arg_shape, indices) -> tuple[int]:
+        sh = np.zeros(arg_shape)[indices].shape
+        return sh
+
+    @pytest.fixture
+    def data_shape(self, arg_shape, sub_shape):
+        return (np.prod(sub_shape), np.prod(arg_shape))
+
+    @pytest.fixture
+    def data_apply(self, arg_shape, indices):
+        arr = np.arange(np.prod(arg_shape)).reshape(arg_shape)
+        out = arr[indices].reshape(-1)
+        return dict(
+            in_=dict(arr=arr.reshape(-1)),
+            out=out,
+        )
+
+
+class TestSubSample(SubSampleMixin, conftest.LinOpT):
     @pytest.fixture(
         params=[
             10,
@@ -17,7 +45,7 @@ class TestSubSample(conftest.LinOpT):
             (10, 5, 2),
         ]
     )
-    def arg_shape(self, request):  # anything which is understood as shape parameter of np.zeros()
+    def arg_shape(self, request):
         return request.param
 
     @pytest.fixture(
@@ -29,13 +57,8 @@ class TestSubSample(conftest.LinOpT):
             [i % 2 == 0 for i in range(10)],  # boolean mask
         ]
     )
-    def indices(self, request):  # anything which can index an NDArray
+    def indices(self, request):
         return (request.param,)
-
-    @pytest.fixture
-    def sub_shape(self, arg_shape, indices) -> tuple[int]:
-        sh = np.zeros(arg_shape)[indices].shape
-        return sh
 
     @pytest.fixture(
         params=itertools.product(
