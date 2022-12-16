a1cd4a57120c96db43186e1799d8731a7a57b5e7
Sepand KASHANI <kashani.sepand@gmail.com>
2022-12-16 15:47:25 +0100

    [pad] Split PadOp into dedicated module.
    base.py is growing too large, and PadOp is not a small piece of code.
    Moreover it embodies a set of classes which may make sense to regroup together down the road.
    Best therefore to move it to a separate module already.

--- src/pycsou/operator/linop/__init__.py
+++ src/pycsou/operator/linop/__init__.py
@@ -1,5 +1,6 @@
 from pycsou.operator.linop.base import *
 from pycsou.operator.linop.kron import *
 from pycsou.operator.linop.nufft import *
+from pycsou.operator.linop.pad import *
 from pycsou.operator.linop.reduce import *
 from pycsou.operator.linop.select import *
