e987820440cef9593d30e5c379b94b4af1b3b122
Sepand KASHANI <sepand@kashani.ch>
2023-02-08 17:14:48 +0100

    [test][solver] Define global math test for all solvers.

--- src/pycsou_tests/opt/solver/conftest.py
+++ src/pycsou_tests/opt/solver/conftest.py
@@ -1,14 +1,71 @@
+import collections.abc as cabc
+import functools
 import itertools
+import operator
 import time
 
 import numpy as np
 import pytest
+import scipy.optimize as sopt
 
 import pycsou.abc as pyca
 import pycsou.runtime as pycrt
 import pycsou.util as pycu
 import pycsou.util.deps as pycd
 import pycsou.util.ptype as pyct
+
+
+def funcs(N: int, seed: int = 0) -> cabc.Sequence[cabc.Sequence[pyct.OpT]]:
+    # Sequence of functional descriptors. (More terms can be added.)
+    #
+    # Used to create strongly-convex functionals.
+    import pycsou.operator.func as pycf
+    import pycsou.operator.linop as pycl
+    import pycsou_tests.operator.examples.test_normalop as normalop
+    import pycsou_tests.operator.examples.test_unitop as unitop
+
+    rng = np.random.default_rng(seed)
+    f1 = (  # f1(x) = \norm{A1 x}{2}^{2}
+        pycf.SquaredL2Norm(dim=N),
+        pycl.HomothetyOp(cst=rng.uniform(1.1, 1.3), dim=N),
+    )
+    f2 = (  # f2(x) = \norm{A2 x - y2}
+        pycf.SquaredL2Norm(dim=N).asloss(rng.uniform(1, 3)),
+        unitop.Permutation(N=N),
+    )
+    f3 = (  # f3(x) = sum(x)
+        pycl.Sum(arg_shape=N),
+        pycl.IdentityOp(dim=N),
+    )
+    return (f1, f2, f3)
+
+
+def generate_funcs(descr, N_term: int) -> cabc.Sequence[tuple[pyct.OpT]]:
+    # Take description of many functionals, i.e. output of funcs(), and return a stream of
+    # length-N_term tuples, where each term of the tuple is a functional created by summing a subset
+    # of `descr`.
+    #
+    # Examples
+    # --------
+    # generate_funcs([(a, b), (c, d), (e, f)], 1)
+    # -> [ (a*b + c*d + e*f,), ]
+    #
+    # generate_funcs([(a, b), (c, d), (e, f)], 2)
+    # -> [ (a*b, c*d + e*f), (a*b + c*d, e*f), (c*d + e*f, a*b), ]
+    assert 1 <= N_term <= len(descr)
+    chain = lambda x, y: x * y
+
+    stream = []
+    for d in itertools.permutations(descr):
+        # split into N_term subsets
+        parts = [chain(*d[i]) for i in range(N_term - 1)]
+
+        # The last subset may contain more than 1 term
+        p = functools.reduce(operator.add, [chain(*dd) for dd in d[N_term - 1 :]])
+        parts.append(p)
+
+        stream.append(tuple(parts))
+    return stream
 
 
 class SolverT:
@@ -37,6 +94,34 @@ class SolverT:
         for k in nd_1.keys():
             stats[k] = nd_1[k].dtype == nd_2[k].dtype
         return all(stats.values())
+
+    @classmethod
+    def _check_value_fit(
+        cls,
+        data: dict[str, pyct.NDArray],
+        cost_func: dict[str, pyct.OpT],
+        ground_truth: dict[str, pyct.NDArray],
+    ):
+        # SciPy ground truth may have converged closer to the solution than `solver` due to
+        # different stopping criteria used.
+        # Since we assume cost-functions are strongly convex, we sidestep potential discrepancies
+        # between ground-truth/solver outputs by comparing the relative difference between their
+        # cost-function values.
+        # We piggy-back onto RelError to simplify code in cases where there are multiple values
+        # being optimized in parallel.
+        from pycsou.opt.stop import RelError
+
+        success = dict()
+        for k, c_func in cost_func.items():
+            crit = RelError(
+                eps=5e-2,  # 5% relative discrepancy tolerated.
+                var=k,
+                f=c_func,
+                satisfy_all=True,
+            )
+            crit.stop(ground_truth)  # load the ground truth
+            success[k] = crit.stop(data)  # then ensure solver output is within tolerated objective-func range
+        assert success
 
     # Fixtures ----------------------------------------------------------------
     @pytest.fixture
@@ -88,7 +173,21 @@ class SolverT:
         slvr = solver_klass(**kwargs_init)
         return slvr
 
-    @pytest.fixture(params=pycd.supported_array_modules())
+    @pytest.fixture(
+        # We do not run solvers with DASK inputs due to general slowness.
+        # This is not a problem in practice: it is mostly operators which
+        # need to be tested for DASK compliance.
+        params=[
+            pycd.NDArrayInfo.NUMPY.module(),
+            pytest.param(
+                pycd.NDArrayInfo.CUPY.module(),
+                marks=pytest.mark.skipif(
+                    not pycd.CUPY_ENABLED,
+                    reason="GPU unsupported on this machine.",
+                ),
+            ),
+        ]
+    )
     def xp(self, request) -> pyct.ArrayModule:
         return request.param
 
@@ -97,10 +196,35 @@ class SolverT:
         return request.param
 
     @pytest.fixture
-    def ground_truth(self, solver, kwargs_fit) -> dict:
-        # The output when computing via NUMPY, assumed correct.
-        solver.fit(**kwargs_fit)
-        data, _ = solver.stats()
+    def cost_function(self, kwargs_init) -> dict[str, pyct.OpT]:
+        # override in subclass to create cost function minimized per logged variable.
+        #
+        # Inputs are assumed to be some linear combination of conftest.funcs().
+        # The cost functions are hence guaranteed to be a strongly convex.
+        raise NotImplementedError
+
+    @pytest.fixture
+    def ground_truth(self, xp, cost_function) -> dict:
+        # Compute the optimal value of the cost functions.
+        #
+        # Must return the same output as solver.stats()[0], i.e. the data dictionary.
+        def fun(x: np.ndarray, cost_f: pyct.OpT) -> (float, np.ndarray):  # f(x), \grad_{f}(x)
+            val = cost_f.apply(x)
+            grad = cost_f.grad(x)
+            return float(val), grad
+
+        rng = np.random.default_rng()
+        data = dict()
+        for (log_var, cost_f) in cost_function.items():
+            res = sopt.minimize(
+                fun=fun,
+                args=(cost_f,),
+                x0=rng.uniform(size=cost_f.dim),
+                method="CG",
+                jac=True,
+            )
+            assert res.success  # if raised, means cost function was not strongly-convex
+            data[log_var] = xp.array(res.x)
         return data
 
     # Tests -------------------------------------------------------------------
@@ -121,11 +245,11 @@ class SolverT:
             stats = {k: v.dtype == width.value for (k, v) in data.items()}
             assert all(stats.values())
 
-    def test_value_fit(self, solver, _kwargs_fit_xp, ground_truth):
+    def test_value_fit(self, solver, _kwargs_fit_xp, cost_function, ground_truth):
         # ensure output computed with backend=xp matches ground_truth NumPy result.
         solver.fit(**_kwargs_fit_xp.copy())
         data, _ = solver.stats()
-        assert self._check_allclose(data, ground_truth)
+        self._check_value_fit(data, cost_function, ground_truth)
 
     def test_transparent_fit(self, solver, kwargs_fit):
         # Running solver twice returns same results.
