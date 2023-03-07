59fd7f371beb4262d06645587ce1c4fb82c4ac28
Sepand KASHANI <sepand@kashani.ch>
2023-03-07 09:07:17 +0100

    [statistics] Comments.
    * Add return-type annotations where applicable.
    * Add .stat() to get estimate without new inputs.
    * Merge update() and _return_update().
    * [TBD] Not in favor of operator arithmetic here.

--- src/pycsou/sampler/statistics.py
+++ src/pycsou/sampler/statistics.py
@@ -13,39 +13,40 @@ class _OnlineStat:
         self._num_samples = 0
         self._stat = None
 
-    def update(self, x: pyct.NDArray):
-        """Update the statistics based on last sample."""
-        out = self._return_update(x)
-        self._stat = out
-        self._num_samples += 1
-        return out
+    def update(self, x: pyct.NDArray) -> pyct.NDArray:
+        """
+        Update the statistics based on last sample.
 
-    def _return_update(self, x: pyct.NDArray):
-        """Returns the updated statistics (to be implemented in subclasses)."""
+        Must be overriden in sub-classes and update `_num_samples` and `_stat`.
+        """
         raise NotImplementedError
+
+    def stat(self) -> pyct.NDArray:
+        """Get current online estimate."""
+        return self._stat
 
     def __add__(self, other: _OnlineStat):
         stat = _OnlineStat()
         stat_update = lambda _, x: self.update(x) + other.update(x)
-        stat._return_update = types.MethodType(stat_update, stat)
+        stat.update = types.MethodType(stat_update, stat)
         return stat
 
     def __sub__(self, other: _OnlineStat):
         stat = _OnlineStat()
         stat_update = lambda _, x: self.update(x) - other.update(x)
-        stat._return_update = types.MethodType(stat_update, stat)
+        stat.update = types.MethodType(stat_update, stat)
         return stat
 
     def __mul__(self, fact: pyct.Real):
         stat = _OnlineStat()
         stat_update = lambda _, x: fact * self.update(x)
-        stat._return_update = types.MethodType(stat_update, stat)
+        stat.update = types.MethodType(stat_update, stat)
         return stat
 
     def __pow__(self, expo: pyct.Real):
         stat = _OnlineStat()
         stat_update = lambda _, x: self.update(x) ** expo
-        stat._return_update = types.MethodType(stat_update, stat)
+        stat.update = types.MethodType(stat_update, stat)
         return stat
 
 
@@ -54,17 +55,17 @@ class OnlineMoment(_OnlineStat):
 
     def __init__(self, order: pyct.Integer = 1):
         super().__init__()
-        self._order = order
+        self._order = int(order)
 
-    def _return_update(self, x: pyct.NDArray) -> pyct.NDArray:
+    def update(self, x: pyct.NDArray) -> pyct.NDArray:
         if self._num_samples == 0:
-            stat = x**self._order
+            self._stat = x**self._order
         else:
-            stat = self._stat
-            stat *= self._num_samples
-            stat += x**self._order
-        stat /= self._num_samples + 1
-        return stat
+            self._stat *= self._num_samples
+            self._stat += x**self._order
+        self._stat /= self._num_samples + 1
+        self._num_samples += 1
+        return self._stat
 
 
 def OnlineVariance():
