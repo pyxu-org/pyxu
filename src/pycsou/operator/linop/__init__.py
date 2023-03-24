a245be6dfe2bb420df41d5c1b5213762611b60c2
Sepand KASHANI <sepand@kashani.ch>
2023-03-24 18:19:38 +0100

    [nufft] Conditionally load library.

--- src/pycsou/operator/linop/__init__.py
+++ src/pycsou/operator/linop/__init__.py
@@ -1,7 +1,11 @@
 from pycsou.operator.linop.base import *
 from pycsou.operator.linop.kron import *
-from pycsou.operator.linop.nufft import *
 from pycsou.operator.linop.pad import *
 from pycsou.operator.linop.reduce import *
 from pycsou.operator.linop.select import *
 from pycsou.operator.linop.stencil import *
+from pycsou.util.inspect import import_module
+
+finufft = import_module("finufft", fail_on_error=False)
+if finufft is not None:
+    from pycsou.operator.linop.nufft import *
