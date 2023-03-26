9cfc222711c41312679f3e40ee32c6b30b7b3856
matthieumeo <matthieu.simeoni@gmail.com>
2023-03-27 00:31:00 +0200

    [interop][pytorch] First working prototype of a Pycsou Map wrapper for PyTorch with autodiff and adjoint features.

---
+++ src/pycsou/interop/_wrapper.py
@@ -0,0 +1,56 @@
+import types
+import typing as typ
+
+import numpy as np
+
+import pycsou.abc.operator as pyco
+import pycsou.runtime as pycrt
+import pycsou.util as pycu
+import pycsou.util.ptype as pyct
+import pycsou.util.warning as pycw
+
+
+class _Wrapper:
+    @pycrt.enforce_precision(i=["lipschitz", "diff_lipschitz"])
+    def __init__(
+        self,
+        op,
+        shape: pyct.OpShape,
+        arg_shape: pyct.NDArrayShape = None,
+        base: pyct.OpT = pyco.Map,
+        lipschitz=np.inf,
+        diff_lipschitz=np.inf,
+        name: str = "PyDataOp",
+        meta: typ.Any = None,
+    ):
+        self._lipschitz = lipschitz
+        self._diff_lipschitz = diff_lipschitz
+        self._pydata_op = op
+        self._base = base
+        self._shape = shape
+        self._arg_shape = arg_shape if arg_shape is not None else pycu.as_canonical_shape(shape[1])
+        self._name = name
+        self._meta = op if meta is None else meta
+
+    def op(self) -> pyct.OpT:
+        if self._base.has((pyco.Property.PROXIMABLE, pyco.Property.QUADRATIC)):
+            raise pycw.AutoInferenceWarning(
+                f"Automatic construction of a {self._base} object from a DL operator is ambiguous."
+            )
+        op = self._base(shape=self._shape)
+        op._pydata_op = self._pydata_op  # embed for introspection
+        op._arg_shape = self._arg_shape
+        op._name = self._name
+        op._meta = self._meta
+        for p in op.properties():
+            for name in p.arithmetic_attributes():
+                attr = getattr(self, name)
+                setattr(op, name, attr)
+            for name in p.arithmetic_methods():
+                func = getattr(self.__class__, name, None)
+                if func is not None:
+                    setattr(op, name, types.MethodType(func, op))  # comment on warning
+        return op
+
+    def _expr(self):
+        return ("from_pydata", self._meta)
