d98f5d6a471225cf2487671884553470b834a42d
Sepand KASHANI <sepand@kashani.ch>
2023-03-31 13:23:57 +0200

    [interop] Add JAX module.

--- src/pycsou/operator/interop/__init__.py
+++ src/pycsou/operator/interop/__init__.py
@@ -1,3 +1,8 @@
 from pycsou.operator.interop.pytorch import *
 from pycsou.operator.interop.sciop import *
 from pycsou.operator.interop.source import *
+from pycsou.util.inspect import import_module
+
+jax = import_module("jax", fail_on_error=False)
+if jax is not None:
+    from pycsou.operator.interop.jax import *
