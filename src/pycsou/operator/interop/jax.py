d821b6441d42a5ec83815939759641d7789097cf
Sepand KASHANI <sepand@kashani.ch>
2023-04-04 05:36:17 +0200

    [jax] Add JAX interop.

---
+++ src/pycsou/operator/interop/jax.py
@@ -0,0 +1,531 @@
+import functools
+import types
+import warnings
+
+import jax
+import jax.dlpack
+import jax.numpy as jnp
+
+import pycsou.abc.operator as pyco
+import pycsou.operator.interop.source as pycsrc
+import pycsou.runtime as pycrt
+import pycsou.util as pycu
+import pycsou.util.deps as pycd
+import pycsou.util.ptype as pyct
+import pycsou.util.warning as pycuw
+
+__all__ = [
+    "from_jax",
+]
+
+
+def _from_jax(
+    x: jax.Array,
+    xp: pyct.ArrayModule = pycd.NDArrayInfo.NUMPY.module(),
+) -> pyct.NDArray:
+    # Zero-copy JAX -> NUMPY/CUPY conversion
+    if xp == pycd.NDArrayInfo.DASK.module():
+        raise pycuw.BackendWarning("DASK inputs are unsupported.")
+
+    y = xp.from_dlpack(x)
+    return y
+
+
+def _to_jax(x: pyct.NDArray, enable_warnings: bool = True) -> jax.Array:
+    # NUMPY/CUPY -> JAX conversion.
+    #
+    # Conversion is zero-copy when possible.
+    N = pycd.NDArrayInfo  # shorthand
+    W = pycrt.Width  # shorthand
+
+    ndi = N.from_obj(x)
+    if ndi == N.DASK:
+        raise pycuw.BackendWarning("DASK inputs are unsupported.")
+    if x.dtype not in [width.value for width in W]:
+        msg = "For safety reasons, _to_jax() only accepts pycsou.runtime.Width-supported dtypes."
+        raise pycuw.PrecisionWarning(msg)
+
+    dev_type = {N.NUMPY: "cpu", N.CUPY: "gpu"}[ndi]
+    dev = jax.devices(dev_type)[0]
+    with jax.default_device(dev):
+        try:
+            x_dl = x.__dlpack__()
+            y = jax.dlpack.from_dlpack(x_dl)
+        except TypeError:
+            if ndi == N.NUMPY:
+                if enable_warnings:
+                    msg = "\n".join(
+                        [
+                            "_to_jax(): a zero-copy conversion did not take place.",
+                            "NUMPY does not allow DLPACK export of read-only arrays.",
+                            "(https://github.com/numpy/numpy/issues/20742)",
+                        ]
+                    )
+                    warnings.warn(msg, pycuw.BackendWarning)
+                y = jnp.asarray(x)
+            else:
+                raise
+
+    if (W(y.dtype) != W(x.dtype)) and enable_warnings:
+        msg = "\n".join(
+            [
+                "_to_jax(): a zero-copy conversion did not take place.",
+                "(https://jax.readthedocs.io/en/latest/notebooks/Common_Gotchas_in_JAX.html#double-64bit-precision)",
+            ]
+        )
+        warnings.warn(msg, pycuw.PrecisionWarning)
+    return y
+
+
+def from_jax(
+    cls: pyct.OpC,
+    shape: pyct.OpShape,
+    vectorize: pyct.VarName = frozenset(),
+    jit: bool = False,
+    enable_warnings: bool = True,
+    **kwargs,
+) -> pyct.OpT:
+    r"""
+    Define an :py:class:`~pycsou.abc.operator.Operator` from low-level constructs.
+
+    Parameters
+    ----------
+    cls: pyct.OpC
+        Operator sub-class to instantiate.
+    shape: pyct.OpShape
+        (N, M) operator shape.
+    kwargs: dict
+        (k[str], v[value | callable]) pairs to use as arithmetic attributes and methods.
+
+        Keys are restricted to the following arithmetic attributes/methods:
+
+        .. code-block:: python3
+
+           _lipschitz, _diff_lipschitz                 # attributes
+           apply(), grad(), prox(), pinv(), adjoint()  # methods
+
+        Omitted arithmetic attributes/methods default to those provided by `cls`, or are
+        auto-inferred via auto-diff rules.
+    vectorize: pyct.VarName
+        Arithmetic methods to vectorize.
+
+        `vectorize` is useful if an arithmetic method provided to `kwargs` does not support stacking
+        dimensions.
+    jit: bool
+        If ``True``, JIT-compile JAX-backed arithmetic methods for better performance.
+    enable_warnings: bool
+        If ``True``, emit warnings in case of precision/zero-copy issues.
+
+    Returns
+    -------
+    op: pyct.OpT
+        (N, M) Pycsou-compliant operator.
+
+    Notes
+    -----
+    * If provided, arithmetic methods must abide exactly to the interface below:
+
+      .. code-block:: python3
+
+         def apply(arr: jax.Array) -> jax.Array
+         def grad(arr: jax.Array) -> jax.Array
+         def adjoint(arr: jax.Array) -> jax.Array
+         def prox(arr: jax.Array, tau: pyct.Real) -> jax.Array
+         def pinv(arr: jax.Array, damp: pyct.Real) -> jax.Array
+
+      Moreover, the methods above MUST accept ``(..., M)``-shaped inputs for ``arr``.
+      If this does not hold, consider populating `vectorize`.
+
+    * Auto-vectorization consists in decorating `kwargs`-specified arithmetic methods with
+      :py:func:`jax.numpy.vectorize`.
+
+    * All arithmetic methods provided in `kwargs` are decorated using
+      :py:func:`~pycsou.runtime.enforce_precision` to abide by Pycsou's FP-runtime semantics.
+      Note however that JAX enforces 32-bit arithmetic by default, and this constraint cannot be
+      changed at runtime.
+      (See https://jax.readthedocs.io/en/latest/notebooks/Common_Gotchas_in_JAX.html#double-64bit-precision .)
+      As such, to allow zero-copy transfers between JAX and NUMPY/CUPY arrays, it is advised to
+      perform computations in single-precision mode.
+      This can be achieved locally using the :py:class:`~pycsou.runtime.Precision` context manager.
+      (See example.)
+      Alternatively, precision/copy-related warnings can be silenced via the `enable_warnings`
+      option of :py:func:`~pycsou.operator.interop.jax.from_jax`.
+
+    * Inferred arithmetic methods are not JIT-ed by default since the operation is error-prone
+      depending on how :py:meth:`~pycsou.abc.operator.Map.apply` is defined.
+      If :py:meth:`~pycsou.abc.operator.Map.apply` supplied to
+      :py:func:`~pycsou.operator.interop.from_jax` is JIT-friendly, then consider enabling `jit`.
+
+    Examples
+    --------
+    Create the custom differential map :math:`f: \mathbb{R}^{2} \to \mathbb{R}^{3}`:
+
+    .. math::
+
+       f(x, y) =
+       \left[
+           \sin(x) + \cos(y),
+           \cos(x) - \sin(y),
+           \sin(x) + \cos(x)
+       \right]
+
+    .. code-block:: python3
+
+       import pycsou.abc as pyca
+       import pycsou.runtime as pycrt
+       import pycsou.operator.interop.jax as pycjax
+       import jax, jax.numpy as jnp, numpy as np
+
+       @jax.jit
+       def j_apply(arr: jax.Array) -> jax.Array:
+           x, y = arr[0], arr[1]
+           o1 = jnp.sin(x) + jnp.cos(y)
+           o2 = jnp.cos(x) - jnp.sin(y)
+           o3 = jnp.sin(x) + jnp.cos(x)
+           out = jnp.r_[o1, o2, o3]
+           return out
+
+       op = pycjax.from_jax(
+           cls=pyca.DiffMap,
+           shape=(3, 2),
+           vectorize="apply",  # j_apply() does not work on stacked inputs
+                               # --> let JAX figure it out automatically.
+           apply=j_apply,
+       )
+       with pycrt.Precision(pycrt.Width.SINGLE):
+           rng = np.random.default_rng(0)
+           x = rng.normal(size=(5,3,4,2))
+           y1 = op.apply(x)  # (5,3,4,3)
+
+           x = rng.normal(size=(2,))
+           opJ = op.jacobian(x)  # JAX auto-infers the Jacobian for you.
+
+           v = rng.normal(size=(5,2))
+           w = rng.normal(size=(4,3))
+           y2 = opJ.apply(v)  # (5,3)
+           y3 = opJ.adjoint(w)  # (4,2)
+    """
+    if isinstance(vectorize, str):
+        vectorize = (vectorize,)
+    vectorize = frozenset(vectorize)
+
+    src = _FromJax(
+        cls=cls,
+        shape=shape,
+        vectorize=vectorize,
+        jit=bool(jit),
+        enable_warnings=bool(enable_warnings),
+        **kwargs,
+    )
+    op = src.op()
+    return op
+
+
+class _FromJax(pycsrc._FromSource):
+    # supported attributes/methods in __init__(**kwargs)
+    _attr = frozenset({"_lipschitz", "_diff_lipschitz"})
+    _meth = frozenset({"apply", "grad", "prox", "pinv", "adjoint"})
+
+    def __init__(  # See from_jax() for a detailed description.
+        self,
+        cls: pyct.OpC,
+        shape: pyct.OpShape,
+        vectorize: frozenset[str],
+        jit: bool,
+        enable_warnings: bool,
+        **kwargs,
+    ):
+        super().__init__(
+            cls=cls,
+            shape=shape,
+            embed=dict(),  # jax-funcs are state-free.
+            vectorize=vectorize,
+            vmethod=None,  # pycsou.util.vectorize() not used for jax-funcs
+            enforce_precision=frozenset(),  # will be applied manually in op() ...
+            **kwargs,
+        )
+        self._enforce_fp = frozenset(self._ekwargs)  # ... based on this list
+        self._jit = jit
+        self._enable_warnings = enable_warnings
+
+        # Only a subset of arithmetic attributes/methods allowed from from_jax().
+        if not (set(self._kwargs) <= self._attr | self._meth):
+            msg_head = "Unsupported arithmetic attributes/methods:"
+            unsupported = set(self._kwargs) - (self._attr | self._meth)
+            msg_tail = ", ".join([f"{name}()" for name in unsupported])
+            raise ValueError(f"{msg_head} {msg_tail}")
+
+    def op(self) -> pyct.OpT:
+        # Idea: modify `**kwargs` from constructor to [when required]:
+        #   1. auto-define omitted methods.            [_infer_missing()]
+        #   2. auto-vectorize via vmap().              [_auto_vectorize()]
+        #   3. JIT & JAX<>NUMPY/CUPY conversions.      [_interface()]
+        #   4. delegate enforce_precision() calls to from_source().
+        self._infer_missing()
+        self._auto_vectorize()
+        j_state, kwargs = self._interface()
+
+        _op = pycsrc.from_source(
+            cls=self._op.__class__,
+            shape=self._op.shape,
+            embed=dict(
+                _jax=j_state,
+                _enable_warnings=self._enable_warnings,
+                _jit=self._jit,
+            ),
+            # vectorize=None,  # see top-level comment.
+            # vmethod=None,    #
+            enforce_precision=self._enforce_fp,
+            **kwargs,
+        )
+        return _op
+
+    def _infer_missing(self):
+        # The following methods must be auto-inferred if missing from `kwargs`:
+        #
+        #     grad(), adjoint()
+        #
+        # Missing methods are auto-inferred via auto-diff rules and added to `_kwargs`.
+        # At the end of _infer_missing(), all jax-funcs required for _interface() have been added to
+        # `_kwargs`.
+        #
+        # Notes
+        # -----
+        # This method does NOT produce vectorized implementations: _auto_vectorize() is responsible
+        # for this.
+        self._vectorize = set(self._vectorize)  # to allow updates below
+
+        nl_difffunc = all(  # non-linear diff-func
+            [
+                self._op.has(pyco.Property.DIFFERENTIABLE_FUNCTION),
+                not self._op.has(pyco.Property.LINEAR),
+            ]
+        )
+        if nl_difffunc and ("grad" not in self._kwargs):
+
+            def f_grad(arr: jax.Array) -> jax.Array:
+                f = self._kwargs["apply"]
+                y, f_vjp = jax.vjp(f, arr)
+                v = jnp.ones_like(y)
+                out = f_vjp(v)[0]  # f_vjp() returns a tuple
+                return out
+
+            self._vectorize.add("grad")
+            self._kwargs["grad"] = f_grad
+
+        non_selfadj = all(  # linear, but not self-adjoint
+            [
+                self._op.has(pyco.Property.LINEAR),
+                not self._op.has(pyco.Property.LINEAR_SELF_ADJOINT),
+            ]
+        )
+        if non_selfadj and ("adjoint" not in self._kwargs):
+
+            def f_adjoint(arr: jax.Array) -> jax.Array:
+                f = self._kwargs["apply"]
+                x = jnp.zeros_like(arr, shape=(self._op.dim,))
+                _, f_vjp = jax.vjp(f, x)
+                out = f_vjp(arr)[0]  # f_vjp() returns a tuple
+                return out
+
+            self._vectorize.add("adjoint")
+            self._kwargs["adjoint"] = f_adjoint
+
+        self._vectorize = frozenset(self._vectorize)
+
+    def _auto_vectorize(self):
+        # Vectorize user-specified [or _infer_missing()-added] arithmetic methods via jax.vmap().
+        #
+        # Modifies `_kwargs` to hold vectorized jax-funcs.
+        vkwargs = dict(  # kwargs to jax.numpy.vectorize()
+            apply=dict(signature="(n)->(m)"),
+            adjoint=dict(signature="(n)->(m)"),
+            grad=dict(signature="(n)->(n)"),
+            prox=dict(signature="(n)->(n)", excluded={1}),
+            pinv=dict(signature="(n)->(m)", excluded={1}),
+        )
+
+        for name in self._kwargs:
+            if name in self._vectorize:
+                func = self._kwargs[name]  # necessarily jax_func
+                vectorize = functools.partial(
+                    jax.numpy.vectorize,
+                    **vkwargs[name],
+                )
+                self._kwargs[name] = vectorize(func)
+
+    def _interface(self):
+        # Arithmetic methods supplied in `kwargs`:
+        #
+        #     * take `jax.Array` inputs
+        #     * do not have the `self` parameter. (Reason: to be JIT-compatible.)
+        #
+        # This method creates modified arithmetic functions to match Pycsou's API, and `state`
+        # required for them to work.
+        #
+        # Returns
+        # -------
+        # j_state: dict
+        #     Jax functions referenced by wrapper arithmetic methods. (See below.)
+        #     These functions are JIT-compiled if specified.
+        # kwargs: dict
+        #     Pycsou-compatible functions which can be submitted to interop.from_source().
+        j_state = dict()
+        for name, obj in self._kwargs.items():
+            if name in self._meth:
+                if self._jit:
+                    obj = jax.jit(obj)
+                j_state[name] = obj  # necessarily jax_func
+
+        kwargs = dict()
+        for name, obj in self._kwargs.items():
+            if name in self._attr:
+                kwargs[name] = obj
+            else:
+                func = getattr(self.__class__, name)
+                kwargs[name] = func  # necessarily a pycsou_func
+
+        # Special cases.
+        # (Reason: not in `_kwargs` [c.f. from_jax() docstring], but need to be inferred.)
+        for name in ("jacobian", "_quad_spec", "asarray", "_expr"):
+            kwargs[name] = getattr(self.__class__, name)
+
+        return j_state, kwargs
+
+    # Wrapper arithmetic methods ----------------------------------------------
+    def apply(self, arr: pyct.NDArray) -> pyct.NDArray:
+        j_arr = _to_jax(arr, enable_warnings=self._enable_warnings)
+        func = self._jax["apply"]
+        j_out = func(j_arr)
+        out = _from_jax(j_out, xp=pycu.get_array_module(arr))
+        return out
+
+    def grad(self, arr: pyct.NDArray) -> pyct.NDArray:
+        j_arr = _to_jax(arr, enable_warnings=self._enable_warnings)
+        func = self._jax["grad"]
+        j_out = func(j_arr)
+        out = _from_jax(j_out, xp=pycu.get_array_module(arr))
+        return out
+
+    def adjoint(self, arr: pyct.NDArray) -> pyct.NDArray:
+        j_arr = _to_jax(arr, enable_warnings=self._enable_warnings)
+        func = self._jax["adjoint"]
+        j_out = func(j_arr)
+        out = _from_jax(j_out, xp=pycu.get_array_module(arr))
+        return out
+
+    def prox(self, arr: pyct.NDArray, tau: pyct.Real) -> pyct.NDArray:
+        j_arr = _to_jax(arr, enable_warnings=self._enable_warnings)
+        func = self._jax["prox"]
+        j_out = func(j_arr, tau)  # positional args only if auto-vectorized.
+        out = _from_jax(j_out, xp=pycu.get_array_module(arr))
+        return out
+
+    def pinv(self, arr: pyct.NDArray, **kwargs) -> pyct.NDArray:
+        j_arr = _to_jax(arr, enable_warnings=self._enable_warnings)
+        func = self._jax["pinv"]
+        damp = kwargs.get("damp", 0)
+        j_out = func(j_arr, damp)  # positional args only if auto-vectorized.
+        out = _from_jax(j_out, xp=pycu.get_array_module(arr))
+        return out
+
+    @pycrt.enforce_precision(i="arr", o=False)
+    def jacobian(self, arr: pyct.NDArray) -> pyct.OpT:
+        try:
+            # Use the class' method if available ...
+            klass = self.__class__
+            op = klass.jacobian(self, arr)
+        except NotImplementedError:
+            # ... and fallback to auto-inference if undefined.
+            f = self._jax["apply"]
+            hint = types.SimpleNamespace(shape=(self.dim,), dtype=arr.dtype)
+
+            j_arr = _to_jax(arr, enable_warnings=self._enable_warnings)
+            _, f_fwd = jax.linearize(f, j_arr)
+            _adj = jax.linear_transpose(f_fwd, hint)
+            f_adj = lambda arr: _adj(arr)[0]  # jax returns a tuple
+
+            klass = pyco.LinFunc if (self.codim == 1) else pyco.LinOp
+            op = from_jax(
+                cls=klass,
+                shape=self.shape,
+                vectorize=("apply", "adjoint"),
+                jit=self._jit,
+                enable_warnings=self._enable_warnings,
+                apply=f_fwd,
+                adjoint=f_adj,
+            )
+        return op
+
+    def _quad_spec(self):
+        if self.has(pyco.Property.QUADRATIC):
+            # auto-infer (Q, c, t)
+            def _grad(arr: jax.Array) -> jax.Array:
+                # Just like jax.grad(f)(arr), but works with (1,)-valued functions.
+                # [jax.grad(f) expects scalar outputs.]
+                f = self._jax["apply"]
+                y, f_vjp = jax.vjp(f, arr)
+                v = jnp.ones_like(y)
+                out = f_vjp(v)[0]  # f_vjp() returns a tuple
+                return out
+
+            # vectorize & JIT internal function
+            _grad = jnp.vectorize(_grad, signature="(m)->(m)")
+            if self._jit:
+                _grad = jax.jit(_grad)
+
+            def Q_apply(arr: jax.Array) -> jax.Array:
+                # \grad_{f}(x) = Q x + c = Q x + \grad_{f}(0)
+                # ==> Q x = \grad_{f}(x) - \grad_{f}(0)
+                z = jnp.zeros_like(arr)
+                out = _grad(arr) - _grad(z)
+                return out
+
+            def c_apply(arr: jax.Array) -> jax.Array:
+                z = jnp.zeros_like(arr)
+                c = _grad(z)
+                out = jnp.sum(c * arr, axis=-1, keepdims=True)
+                return out
+
+            Q = from_jax(
+                cls=pyco.PosDefOp,
+                shape=(self.dim, self.dim),
+                vectorize="apply",
+                jit=self._jit,
+                enable_warnings=self._enable_warnings,
+                apply=Q_apply,
+            )
+            c = from_jax(
+                cls=pyco.LinFunc,
+                shape=(1, self.dim),
+                vectorize="apply",
+                jit=self._jit,
+                enable_warnings=self._enable_warnings,
+                apply=c_apply,
+            )
+            f = self._jax["apply"]
+            t = float(f(jnp.zeros(self.dim)))
+
+            return (Q, c, t)
+        else:
+            raise NotImplementedError
+
+    def asarray(self, **kwargs) -> pyct.NDArray:
+        if self.has(pyco.Property.LINEAR):
+            # Lin[Op,Func].asarray() assumes the operator is precision-agnostic.
+            # This condition does not hold for JAX arrays. (See from_jax() notes.)
+            #
+            # Consequence: may need to cast Lin[Op,Func].asarray()'s output.
+            klass = self.__class__
+            A = klass.asarray(self, **kwargs)
+
+            dtype = kwargs.get("dtype", pycrt.getPrecision().value)
+            A = A.astype(dtype=dtype, copy=False)
+            return A
+        else:
+            raise NotImplementedError
+
+    def _expr(self) -> tuple:
+        # show which arithmetic methods are backed by jax-funcs
+        jax_funcs = tuple(self._jax.keys())
+        return ("from_jax", *jax_funcs)
