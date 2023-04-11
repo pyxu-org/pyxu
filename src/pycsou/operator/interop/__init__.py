82b2e89095099ecd5157e2430a4d413e2e5d2fbf
Sepand KASHANI <sepand@kashani.ch>
2023-04-11 16:40:29 +0200

    [interop] Simplify top-level __init__.

--- src/pycsou/operator/interop/__init__.py
+++ src/pycsou/operator/interop/__init__.py
@@ -1,8 +1,4 @@
+from pycsou.operator.interop.jax import *
 from pycsou.operator.interop.pytorch import *
 from pycsou.operator.interop.sciop import *
 from pycsou.operator.interop.source import *
-from pycsou.util.inspect import import_module
-
-jax = import_module("jax", fail_on_error=False)
-if jax is not None:
-    from pycsou.operator.interop.jax import *
