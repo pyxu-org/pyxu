2a857b6e6e8fa5c6ed0f8f2a6a85ebb072e56fd1
Sepand KASHANI <sepand@kashani.ch>
2023-04-20 16:19:06 +0200

    [SWE] setup.py restores initial setup.cfg config after install.

--- setup.py
+++ setup.py
@@ -10,11 +10,21 @@ import pathlib as plib
 import setuptools
 
 
-def update_extra_requires():
-    cfg_file = plib.Path(__file__).parent / "setup.cfg"
+def read_file(path: plib.Path) -> str:
+    with open(path, mode="r") as f:
+        txt = f.read()
+    return txt
 
+
+def write_file(path: plib.Path, txt: str):
+    with open(path, mode="w") as f:
+        f.write(txt)
+
+
+def update_extra_requires(cfg_path: plib.Path):
+    # overwrite .CFG file with added targets `complete_gpu`, `complete_no_gpu`.
     cfg = configparser.ConfigParser()
-    with open(cfg_file, mode="r") as f:
+    with open(cfg_path, mode="r") as f:
         cfg.read_file(f)
 
     # Add aggregate targets
@@ -35,9 +45,14 @@ def update_extra_requires():
     xtra["complete_no_gpu"] = "".join(pkg["no_gpu"])
     xtra["complete_gpu"] = "".join(pkg["gpu"])
 
-    with open(cfg_file, mode="w") as f:
+    with open(cfg_path, mode="w") as f:
         cfg.write(f)
 
 
-update_extra_requires()
+cfg_path = plib.Path(__file__).parent / "setup.cfg"
+cfg_init = read_file(cfg_path)  # Save setup.cfg original state
+update_extra_requires(cfg_path)
+
 setuptools.setup(setup_requires=["pbr"], pbr=True)
+
+write_file(cfg_path, cfg_init)  # Restore setup.cfg to original state
