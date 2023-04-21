0bf193ac3c5585b9663705626f2c9e2ac22d104f
matthieumeo <matthieu.simeoni@gmail.com>
2023-04-21 17:10:20 +0200

    [interop] Torch-realted imports are now performed in the torch.py module.

--- src/pycsou/operator/interop/__init__.py
+++ src/pycsou/operator/interop/__init__.py
@@ -1,4 +1,4 @@
 from pycsou.operator.interop.jax import *
-from pycsou.operator.interop.pytorch import *
 from pycsou.operator.interop.sciop import *
 from pycsou.operator.interop.source import *
+from pycsou.operator.interop.torch import *
