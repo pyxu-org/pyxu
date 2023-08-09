5b6fe0941f8063b3691ed8f6382633a3d2ec404d
Joan Rue Queralt <joan.rue.q@gmail.com>
2023-08-09 16:57:03 +0200

    [loss/KLdivergence] finish tests

--- src/pycsou_tests/operator/func/test_loss.py
+++ src/pycsou_tests/operator/func/test_loss.py
@@ -1,11 +1,14 @@
 import itertools
 
 import numpy as np
+import numpy.random as npr
 import pytest
 
 import pycsou.operator.func as pycof
 import pycsou.runtime as pycrt
+import pycsou.util as pycu
 import pycsou.util.deps as pycd
+import pycsou.util.ptype as pyct
 import pycsou_tests.operator.conftest as conftest
 
 
@@ -14,7 +17,7 @@ class TestKLDivergence(conftest.ProxFuncT):
         params=itertools.product(
             # dim
             (5,),
-            pycd.NDArrayInfo,
+            list(pycd.NDArrayInfo),
             pycrt.Width,
         )
     )
@@ -22,13 +25,14 @@ class TestKLDivergence(conftest.ProxFuncT):
         dim = request.param[0]
         ndi = request.param[1]
         dtype = request.param[2].value.name
+        data = np.arange(1, dim + 1, dtype=dtype)
         if ndi is None:
             pytest.skip(f"{ndi} unsupported on this machine.")
             op = None
         else:
             xp = ndi.module()
-            op = pycof.KLDivergence(dim, xp.arange(dim, dtype=dtype))
-        return (dim, op), ndi, request.param[2]
+            op = pycof.KLDivergence(dim, xp.array(data))
+        return (dim, op), ndi, request.param[2], data
 
     @pytest.fixture
     def spec(self, _spec):
@@ -39,46 +43,66 @@ class TestKLDivergence(conftest.ProxFuncT):
         return _spec[0][0]
 
     @pytest.fixture
+    def kl_data(self, _spec):
+        return _spec[3]
+
+    @pytest.fixture
     def data_shape(self, dim):
         return (1, dim)
 
-    @pytest.fixture(
-        params=[  # 2 evaluation points
-            dict(
-                in_=dict(arr=np.zeros((5,))),
-                out=np.array([-10.0]),
-            ),
-            dict(
-                in_=dict(arr=np.arange(-3, 2)),
-                out=np.array([-9.45482256]),
-            ),
-        ]
-    )
-    def data_apply(self, request):
-        return request.param
+    @pytest.fixture(params=[np.zeros((5,)) + 1e-6, np.arange(1, 6)])  # 2 evaluation points
+    def data_apply(self, request, kl_data):
+        x = request.param
+        y = pycu.to_NUMPY(kl_data)
+
+        H = np.ones_like(x) * np.inf
+        ids_1 = (x > 0) * (y > 0)
+        ids_2 = (x == 0) * (y > 0)
+        H[ids_1] = y[ids_1] * np.log(y[ids_1] / x[ids_1])
+        H[ids_2] = 0
+        out = np.sum(H - y + x)
+        return dict(
+            in_=dict(arr=x),
+            out=out,
+        )
 
     @pytest.fixture(
         params=[  # 2 evaluation points
             dict(
-                in_=dict(
-                    arr=np.zeros((5,)),
-                    tau=1,
-                ),
-                out=np.array([0.0, 0.61803399, 1.0, 1.30277564, 1.56155281]),
+                arr=np.zeros((5,)) + 1e-6,
+                tau=1,
             ),
             dict(
-                in_=dict(
-                    arr=np.arange(-3, 2),
-                    tau=1,
-                ),
-                out=np.array([0.0, 0.30277564, 0.73205081, 1.30277564, 2.0]),
+                arr=np.arange(1, 6),
+                tau=1,
             ),
         ]
     )
-    def data_prox(self, request):
-        return request.param
+    def data_prox(self, request, kl_data):
+        # ground truth prox taken from Pycsou v1.
+        in_ = request.param
+        arr = in_["arr"]
+        tau = in_["tau"]
+        out = (arr - tau + np.sqrt((arr - tau) ** 2 + 4 * tau * kl_data)) / 2
+        return dict(in_=in_, out=out)
+
+    @staticmethod
+    def _random_array(
+        shape: pyct.NDArrayShape,
+        seed: int = 0,
+        xp: pyct.ArrayModule = pycd.NDArrayInfo.NUMPY.module(),
+        width: pycrt.Width = pycrt.Width.DOUBLE,
+    ):
+        # Create only arrays with only positive-valued entries.
+        rng = npr.default_rng(seed)
+        x = rng.normal(size=shape)
+        return xp.abs(xp.array(x, dtype=width.value)) + 1e-6
 
     @pytest.fixture
     def data_math_lipschitz(self, dim):
         N_test, dim = 10, self._sanitize(dim, 3)
         return self._random_array((N_test, dim))
+
+    @pytest.mark.skip("KLDiv is already a loss.")
+    def test_interface_asloss(self, op, xp, width):
+        pass
