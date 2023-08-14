[BugFix][stencil] Allow no_eval Lipschitz computation for mode=reflect.
Reflect mode uses slightly less padding than others such that its Lipschitz
upper bound requires division by (N-2), which may yield a divide-by-zero error.

We therefore increase the test kernel to size (3,) per dimension.

src/pycsou/operator/linop/stencil/stencil.py
@@ -540,7 +540,7 @@ class Stencil(pyca.SquareOp):
             _center[np.diag_indices(N)] = center
 
         _mode = Pad(  # get `mode` in canonical form
-            (2,) * _kernel[0].ndim,
+            (3,) * _kernel[0].ndim,
             pad_width=1,
             mode=mode,
         )._mode