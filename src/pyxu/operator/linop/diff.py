4947810e56ef1a28f3a3a7d56cc755a11f672548
Sepand KASHANI <sepand@kashani.ch>
2023-08-29 15:51:09 +0200

    [doc][diff][BugFix] Render plots correctly.

--- src/pyxu/operator/linop/diff.py
+++ src/pyxu/operator/linop/diff.py
@@ -878,8 +878,6 @@ class PartialDerivative:
            plt.imshow(out2.T)
            plt.axis('off')
            plt.title(r'$\frac{\partial^{3} f(x,y)}{\partial x\partial y^{2}}$')
-           # Test
-           assert np.allclose(out1, out2)
 
         """
         assert isinstance(order, cabc.Sequence), "`order` should be a tuple / list"
@@ -1373,17 +1371,19 @@ def Jacobian(
        import matplotlib.pyplot as plt
        from pyxu.operator import Jacobian
        from pyxu.util.misc import peaks
+
        x = np.linspace(-2.5, 2.5, 25)
        xx, yy = np.meshgrid(x, x)
        image = np.tile(peaks(xx, yy), (3, 1, 1))
        jac = Jacobian(arg_shape=image.shape[1:], n_channels=image.shape[0])
        out = jac.unravel(jac(image.ravel()))
        fig, axes = plt.subplots(3, 2, figsize=(10, 15))
-       for i in range(2):
-           for j in range(3):
-               axes[j, i].imshow(out[i, j].T, cmap=["Reds", "Greens", "Blues"][j])
-               axes[j, i].set_title(f"$\partial I_{{{['R', 'G', 'B'][j]}}}/\partial{{{['x', 'y'][i]}}}$")
+       for i in range(3):
+           for j in range(2):
+               axes[i, j].imshow(out[i, j].T, cmap=["Reds", "Greens", "Blues"][j])
+               axes[i, j].set_title(f"$\partial I_{{{['R', 'G', 'B'][j]}}}/\partial{{{['x', 'y'][j]}}}$")
        plt.suptitle("Jacobian")
+
 
     See Also
     --------
@@ -1518,7 +1518,7 @@ def Divergence(
 
        import numpy as np
        import matplotlib.pyplot as plt
-       from pyxu.operator import Divergence, Jacobian, Laplacian
+       from pyxu.operator import Gradient, Divergence, Laplacian
        from pyxu.util.misc import peaks
 
        n = 100
