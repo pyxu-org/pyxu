55eda4899eb0e22a376c7e7e5130c3ad5ffd89da
Joan Rue Queralt <joan.rue.q@gmail.com>
2023-09-12 18:50:06 +0200

    [entrypoints] linterns reformatting

--- src/pyxu/operator/interop/__init__.py
+++ src/pyxu/operator/interop/__init__.py
@@ -4,5 +4,4 @@ from pyxu.operator.interop.sciop import *
 from pyxu.operator.interop.source import *
 from pyxu.operator.interop.torch import *
 
-
 _load_entry_points(globals(), group="pyxu.operator.interop")
