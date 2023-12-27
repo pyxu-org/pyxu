00f6aa8260cac31571ddcee54889108aa73d6f7f
Sepand KASHANI <sepand@kashani.ch>
2023-12-27 18:41:41 +0100

    [info/warn] Add warn_dask_perf() helper.
    Many Pyxu routines are compatible with DASK inputs, but this does not mean they perform well at scale due to DASK or algorithm limitations.
    This function is useful to have standardized DASK-scale-related errors across Pyxu.

--- src/pyxu/info/warning.py
+++ src/pyxu/info/warning.py
@@ -1,4 +1,6 @@
 # Custom warnings used inside Pyxu.
+import inspect
+import warnings
 
 
 class PyxuWarning(UserWarning):
@@ -17,6 +19,29 @@ class PerformanceWarning(PyxuWarning):
     """
     Use for performance-related warnings.
     """
+
+
+def warn_dask_perf(msg: str = None):
+    """
+    Issue a warning for DASK-related performance issues.
+
+    This method is aware of its context and prints the name of the enclosing function/method which invoked it.
+
+    Parameters
+    ----------
+    msg: str
+        Custom warning message.
+    """
+    if msg is None:
+        msg = "Sub-optimal performance for DASK inputs."
+
+    # Get context
+    my_frame = inspect.currentframe()
+    up_frame = inspect.getouterframes(my_frame)[1]
+    header = f"{up_frame.filename}:{up_frame.function}"
+
+    msg = f"[{header}] {msg}"
+    warnings.warn(msg, PerformanceWarning)
 
 
 class PrecisionWarning(PyxuWarning):
