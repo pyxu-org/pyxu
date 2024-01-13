31a735e5c633472d3fb5efa4de3e8eb56f56c4ec
Sepand KASHANI <sepand@kashani.ch>
2024-01-13 21:09:59 +0100

    [op/blocks][stack] Refactor to ND API.
    Parallel execution of arithmetic methods via DASK is dropped for NUMPY/CUPY inputs.
    Reason: CompositeOperator() will solve this down the road, so no point adding extra complexity right now.
    
    Due to ND in/out assumption, stack() cannot accept arbitrary dim/codim shapes; see docstrings for details.

--- src/pyxu/operator/blocks.py
+++ src/pyxu/operator/blocks.py
@@ -1,8 +1,11 @@
 import collections.abc as cabc
 import types
 
+import numpy as np
+
 import pyxu.abc as pxa
 import pyxu.info.ptype as pxt
+import pyxu.operator.interop.source as px_src
 import pyxu.runtime as pxrt
 import pyxu.util as pxu
 from pyxu.info.plugin import _load_entry_points
@@ -15,11 +18,68 @@ __all__ = [
 __all__ = _load_entry_points(globals(), group="pyxu.opt.blocks", names=__all__)
 
 
-def stack(
-    ops: cabc.Sequence[pxt.OpT],
-    axis: pxt.Integer,
-) -> pxt.OpT:
-    pass
+def stack(ops: cabc.Sequence[pxt.OpT]) -> pxt.OpT:
+    r"""
+    Map operators over the same input.
+
+    A stacked operator :math:`S: \mathbb{R}^{M_{1} \times\cdots\times M_{D}} \to \mathbb{R}^{Q \times N_{1}
+    \times\cdots\times N_{K}} is an operator containing (vertically) :math:`Q` blocks of smaller operators :math:`\{
+    O_{q}: \mathbb{R}^{M_{1} \times\cdots\times M_{D}} \to \mathbb{R}^{N_{1} \times\cdots\times N_{K}} \}_{q=1}^{Q}`:
+
+    .. math::
+
+       S
+       =
+       \left[
+           \begin{array}{c}
+               O_{1}  \\
+               \vdots \\
+               O_{Q}  \\
+           \end{array}
+       \right]
+
+    Each sub-operator :math:`O_{q}` acts on the same input and returns parallel outputs which get stacked along the
+    zero-th axis.
+
+    Parameters
+    ----------
+    ops: :py:class:`~collections.abc.Sequence` ( :py:attr:`~pyxu.info.ptype.OpT` )
+        (Q,) identically-shaped operators to map over inputs.
+
+    Returns
+    -------
+    op: OpT
+        Stacked (M1,...,MD) -> (Q, N1,...,NK) operator.
+
+    Examples
+    --------
+
+    .. code-block:: python3
+
+       import pyxu.operator as pxo
+       import numpy as np
+
+       op = pxo.Sum((3, 4), axis=-1)  # (3,4) -> (3,1)
+       A = pxo.stack([op, 2*op])  # (3,4) -> (2,3,1)
+
+       x = np.arange(A.dim_size).reshape(A.dim_shape)  # [[ 0  1  2  3]
+                                                       #  [ 4  5  6  7]
+                                                       #  [ 8  9 10 11]]
+       y = A.apply(x)  # [[[ 6.]
+                       #   [22.]
+                       #   [38.]]
+                       #
+                       #  [[12.]
+                       #   [44.]
+                       #   [76.]]]
+
+
+    See Also
+    --------
+    :py:func:`~pyxu.operator.block_diag`
+    """
+    op = _Stack(ops).op()
+    return op
 
 
 def block_diag(ops: cabc.Sequence[pxt.OpT]) -> pxt.OpT:
@@ -262,3 +322,153 @@ class _BlockDiag:
 
     def _expr(self) -> tuple:
         return ("block_diag", *self._ops)
+
+
+class _Stack:
+    # See stack() docstrings.
+    def __init__(self, ops: cabc.Sequence[pxt.OpT]):
+        assert len(ops) > 1
+
+        dim_shape = ops[0].dim_shape
+        codim_shape = ops[0].codim_shape
+
+        shape_msg = "All operators must have same dim/codim."
+        assert all(_op.dim_shape == dim_shape for _op in ops), shape_msg
+        assert all(_op.codim_shape == codim_shape for _op in ops), shape_msg
+
+        self._ops = list(ops)
+
+    def op(self) -> pxt.OpT:
+        klass = self._infer_op_klass()
+        N_op = len(self._ops)
+        dim_shape = self._ops[0].dim_shape
+        codim_shape = self._ops[0].codim_shape
+        op = klass(
+            dim_shape=dim_shape,
+            codim_shape=(N_op, *codim_shape),
+        )
+        op._ops = self._ops  # embed for introspection
+        for p in op.properties():
+            for name in p.arithmetic_methods():
+                func = getattr(self.__class__, name, None)
+                if func is not None:
+                    setattr(op, name, types.MethodType(func, op))
+        self._propagate_constants(op)
+        return op
+
+    def _infer_op_klass(self) -> pxt.OpC:
+        base = {
+            pxa.Property.CAN_EVAL,
+            pxa.Property.DIFFERENTIABLE,
+            pxa.Property.LINEAR,
+        }
+        properties = set.intersection(
+            base,
+            *[_op.properties() for _op in self._ops],
+        )
+        klass = pxa.Operator._infer_operator_type(properties)
+        return klass
+
+    @staticmethod
+    def _propagate_constants(op: pxt.OpT):
+        # Propagate (diff-)Lipschitz constants forward via special call to
+        # Rule()-overridden `estimate_[diff_]lipschitz()` methods.
+
+        # Important: we write to _[diff_]lipschitz to not overwrite estimate_[diff_]lipschitz() methods.
+        if op.has(pxa.Property.CAN_EVAL):
+            op._lipschitz = op.estimate_lipschitz(__rule=True)
+        if op.has(pxa.Property.DIFFERENTIABLE):
+            op._diff_lipschitz = op.estimate_diff_lipschitz(__rule=True)
+
+    @pxrt.enforce_precision(i="arr")
+    def apply(self, arr: pxt.NDArray) -> pxt.NDArray:
+        parts = [_op.apply(arr) for _op in self._ops]
+
+        xp = pxu.get_array_module(arr)
+        out = xp.stack(parts, axis=-self.codim_rank)
+        return out
+
+    def __call__(self, arr: pxt.NDArray) -> pxt.NDArray:
+        return self.apply(arr)
+
+    @pxrt.enforce_precision(i="arr")
+    def adjoint(self, arr: pxt.NDArray) -> pxt.NDArray:
+        N_stack = len(arr.shape[: -self.codim_rank])
+        select = lambda i: (slice(None),) * N_stack + (i,)
+        parts = [_op.adjoint(arr[select(i)]) for (i, _op) in enumerate(self._ops)]
+
+        out = sum(parts)
+        return out
+
+    def jacobian(self, arr: pxt.NDArray) -> pxt.OpT:
+        if self.has(pxa.Property.LINEAR):
+            J = self
+        else:
+            parts = [_op.jacobian(arr) for _op in self._ops]
+            J = _Stack(ops=parts).op()
+        return J
+
+    def asarray(self, **kwargs) -> pxt.NDArray:
+        parts = [_op.asarray(**kwargs) for _op in self._ops]
+        xp = pxu.get_array_module(parts[0])
+        A = xp.stack(parts, axis=0)
+        return A
+
+    def gram(self) -> pxt.OpT:
+        # [_ops.gram()] should be reduced (via +) to form a single operator.
+        # It is inefficient however to chain so many operators together via AddRule().
+        # apply() is thus redefined to improve performance.
+
+        @pxrt.enforce_precision(i="arr")
+        def op_apply(_, arr: pxt.NDArray) -> pxt.NDArray:
+            G = [_op.gram() for _op in _._ops]
+            parts = [_G.apply(arr) for _G in G]
+
+            out = sum(parts)
+            return out
+
+        def op_expr(_) -> tuple:
+            return ("gram", self)
+
+        G = px_src.from_source(
+            cls=pxa.SelfAdjointOp,
+            dim_shape=self.dim_shape,
+            codim_shape=self.dim_shape,
+            embed=dict(_ops=self._ops),
+            apply=op_apply,
+            _expr=op_expr,
+        )
+        return G
+
+    def estimate_lipschitz(self, **kwargs) -> pxt.Real:
+        no_eval = "__rule" in kwargs
+        if no_eval:
+            L_parts = [_op.lipschitz for _op in self._ops]
+        elif self.has(pxa.Property.LINEAR):
+            L = self.__class__.estimate_lipschitz(self, **kwargs)
+            return L
+        else:
+            L_parts = [_op.estimate_lipschitz(**kwargs) for _op in self._ops]
+
+        # [non-linear case] Upper bound: L**2 <= sum(L_k**2)
+        L2 = np.r_[L_parts] ** 2
+        L = np.sqrt(L2.sum())
+        return L
+
+    def estimate_diff_lipschitz(self, **kwargs) -> pxt.Real:
+        no_eval = "__rule" in kwargs
+        if no_eval:
+            dL_parts = [_op.diff_lipschitz for _op in self._ops]
+        elif self.has(pxa.Property.LINEAR):
+            dL = 0
+            return dL
+        else:
+            dL_parts = [_op.estimate_diff_lipschitz(**kwargs) for _op in self._ops]
+
+        # [non-linear case] Upper bound: dL**2 <= sum(dL_k**2)
+        dL2 = np.r_[dL_parts] ** 2
+        dL = np.sqrt(dL2.sum())
+        return dL
+
+    def _expr(self) -> tuple:
+        return ("stack", *self._ops)
---
+++ src/pyxu_tests/operator/blocks/test_stack.py
@@ -0,0 +1,260 @@
+# How stack() tests work:
+#
+# * StackMixin auto-defines all (input,output) pairs.
+#   [Caveat: we assume the base operators (op_all) are correctly implemented.]
+#   (True if choosing test operators from examples/.)
+#
+# * To test a stack-ed operator, inherit from StackMixin and the suitable conftest.MapT subclass
+#   which the compound operator should abide by.
+
+import collections.abc as cabc
+import itertools
+
+import numpy as np
+import pytest
+
+import pyxu.abc as pxa
+import pyxu.info.deps as pxd
+import pyxu.info.ptype as pxt
+import pyxu.operator as pxo
+import pyxu.runtime as pxrt
+import pyxu_tests.operator.conftest as conftest
+
+
+def spec_op(klass: pxt.OpC, N: int = 2) -> list[list[pxt.OpT]]:
+    # create all possible (op1, ..., opN) tuples which, when used to create a stacked operator,
+    # produce an operator of type `klass`.
+    def op_map(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_map as tc
+
+        return tc.ReLU(dim_shape=dim_shape)
+
+    def op_func(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_func as tc
+
+        return tc.Median(dim_shape=dim_shape)
+
+    def op_diffmap(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_diffmap as tc
+
+        return tc.Sin(dim_shape=dim_shape)
+
+    def op_difffunc(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_difffunc as tc
+
+        return tc.SquaredL2Norm(dim_shape=dim_shape)
+
+    def op_proxfunc(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_proxfunc as tc
+
+        return tc.L1Norm(dim_shape=dim_shape)
+
+    def op_proxdifffunc(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_proxdifffunc as tc
+
+        return tc.SquaredL2Norm(dim_shape=dim_shape)
+
+    def op_quadraticfunc(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        # QuadraticFunc may be defined for dim=1.
+        # In this case we cannot use PSDConvolution (examples/test_posdefop.py) due to minimal domain-size restrictions.
+        # We therefore use HomothetyOp without loss of generality.
+
+        from pyxu.operator import HomothetyOp
+        from pyxu_tests.operator.examples.test_linfunc import Sum
+
+        return pxa.QuadraticFunc(
+            dim_shape=dim_shape,
+            codim_shape=1,
+            Q=HomothetyOp(dim_shape=dim_shape, cst=3),
+            c=Sum(dim_shape=dim_shape),
+            t=1,
+        )
+
+    def op_linop(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_linop as tc
+
+        return tc.Sum(dim_shape=dim_shape)
+
+    def op_linfunc(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_linfunc as tc
+
+        op = tc.Sum(dim_shape=dim_shape)
+        return op
+
+    def op_squareop(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_squareop as tc
+
+        return tc.CumSum(dim_shape=dim_shape)
+
+    def op_normalop(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_normalop as tc
+
+        rng = np.random.default_rng(seed=2)
+        conv_filter = rng.normal(size=dim_shape[-1])
+        return tc.CircularConvolution(
+            dim_shape=dim_shape,
+            h=conv_filter,
+        )
+
+    def op_unitop(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_unitop as tc
+
+        return tc.Permutation(dim_shape=dim_shape)
+
+    def op_selfadjointop(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_selfadjointop as tc
+
+        return tc.SelfAdjointConvolution(dim_shape=dim_shape)
+
+    def op_posdefop(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_posdefop as tc
+
+        return tc.PSDConvolution(dim_shape=dim_shape)
+
+    def op_projop(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_projop as tc
+
+        return tc.Oblique(
+            dim_shape=dim_shape,
+            alpha=np.pi / 4,
+        )
+
+    def op_orthprojop(dim_shape: pxt.NDArrayShape) -> pxt.OpT:
+        import pyxu_tests.operator.examples.test_orthprojop as tc
+
+        return tc.ScaleDown(dim_shape=dim_shape)
+
+    def condition(ops: list[pxt.OpT], klass: pxt.OpC) -> bool:
+        # Return true if stack(ops) forms a klass object. [Not a sub-type]
+        properties = frozenset.intersection(*[op.properties() for op in ops]) & {
+            pxa.Property.CAN_EVAL,
+            pxa.Property.DIFFERENTIABLE,
+            pxa.Property.LINEAR,
+        }
+        properties = set(properties)
+
+        _klass = pxa.Operator._infer_operator_type(properties)
+        return _klass == klass
+
+    ops = []
+    for _ops in itertools.combinations_with_replacement(
+        [
+            op_func((5, 3, 5)),
+            op_difffunc((5, 3, 5)),
+            op_proxfunc((5, 3, 5)),
+            op_proxdifffunc((5, 3, 5)),
+            op_linfunc((5, 3, 5)),
+            op_quadraticfunc((5, 3, 5)),
+        ],
+        N,
+    ):
+        if condition(_ops, klass):
+            ops.append(_ops)
+    for _ops in itertools.combinations_with_replacement(
+        [
+            op_map((5, 3, 5)),
+            op_diffmap((5, 3, 5)),
+            op_squareop((5, 3, 5)),
+            op_normalop((5, 3, 5)),
+            op_unitop((5, 3, 5)),
+            op_selfadjointop((5, 3, 5)),
+            op_posdefop((5, 3, 5)),
+            op_projop((5, 3, 5)),
+            op_orthprojop((5, 3, 5)),
+        ],
+        N,
+    ):
+        if condition(_ops, klass):
+            ops.append(_ops)
+    return ops
+
+
+# Data Mixin ------------------------------------------------------------------
+class StackMixin:
+    # Fixtures ----------------------------------------------------------------
+    @pytest.fixture
+    def op_all(self) -> list[pxt.OpT]:
+        # Override in inherited class with sub-operators.
+        raise NotImplementedError
+
+    @pytest.fixture(
+        params=itertools.product(
+            pxd.NDArrayInfo,
+            pxrt.Width,
+        )
+    )
+    def spec(self, op_all, request) -> tuple[pxt.OpT, pxd.NDArrayInfo, pxrt.Width]:
+        ndi, width = request.param
+        op = pxo.stack(op_all)
+        return op, ndi, width
+
+    @pytest.fixture
+    def dim_shape(self, op_all) -> pxt.NDArrayShape:
+        dim_shape0 = op_all[0].dim_shape
+        return dim_shape0
+
+    @pytest.fixture
+    def codim_shape(self, op_all) -> pxt.NDArrayShape:
+        N_op = len(op_all)
+        codim_shape0 = op_all[0].codim_shape
+        return (N_op, *codim_shape0)
+
+    @pytest.fixture
+    def data_apply(self, dim_shape, op_all) -> conftest.DataLike:
+        x = self._random_array(dim_shape)
+
+        y = [None] * len(op_all)
+        for i in range(len(op_all)):
+            y[i] = op_all[i].apply(x)
+        y = np.stack(y, axis=0)
+
+        return dict(
+            in_=dict(arr=x),
+            out=y,
+        )
+
+    @pytest.fixture
+    def data_adjoint(self, codim_shape, op_all) -> conftest.DataLike:
+        x = self._random_array(codim_shape)
+
+        y = 0
+        for i in range(len(op_all)):
+            y += op_all[i].adjoint(x[i])
+
+        return dict(
+            in_=dict(arr=x),
+            out=y,
+        )
+
+    @pytest.fixture
+    def data_math_lipschitz(self, dim_shape) -> cabc.Collection[np.ndarray]:
+        N_test = 20
+        x = self._random_array((N_test, *dim_shape))
+        return x
+
+    @pytest.fixture
+    def data_math_diff_lipschitz(self, dim_shape) -> cabc.Collection[np.ndarray]:
+        N_test = 20
+        x = self._random_array((N_test, *dim_shape))
+        return x
+
+    # Tests -------------------------------------------------------------------
+
+
+# Test classes (Maps) ---------------------------------------------------------
+class TestStackMap(StackMixin, conftest.MapT):
+    @pytest.fixture(params=spec_op(pxa.Map))
+    def op_all(self, request):
+        return request.param
+
+
+class TestStackDiffMap(StackMixin, conftest.DiffMapT):
+    @pytest.fixture(params=spec_op(pxa.DiffMap))
+    def op_all(self, request):
+        return request.param
+
+
+class TestStackLinOp(StackMixin, conftest.LinOpT):
+    @pytest.fixture(params=spec_op(pxa.LinOp))
+    def op_all(self, request):
+        return request.param
