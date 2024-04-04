ecbc4a487532a83ba1c29100e1f09157d0543fa3
Joan Rue Queralt <joan.rue.q@gmail.com>
2024-04-04 17:40:35 +0200

    [doc/pds] replace operators in examples from _dev for core operators

--- src/pyxu/opt/solver/pds.py
+++ src/pyxu/opt/solver/pds.py
@@ -397,29 +397,27 @@ class CondatVu(_PrimalDualSplitting):
        import matplotlib.pyplot as plt
        import numpy as np
        import pyxu.operator as pxo
-       from pyxu.experimental._dev import DownSampling, FirstDerivative
+       from pyxu.operator import SubSample, PartialDerivative
        from pyxu.opt.solver import CV
 
        x = np.repeat(np.asarray([0, 2, 1, 3, 0, 2, 0]), 10)
        N = x.size
 
-       D = FirstDerivative(size=N, kind="forward")
-       D.lipschitz = D.estimate_lipschitz()
+       D = PartialDerivative.finite_difference(dim_shape=(N,), order=(1,))
 
-       downsample = DownSampling(size=N, downsampling_factor=3)
+       downsample = SubSample(N, slice(None, None, 3))
        y = downsample(x)
        loss = (1 / 2) * pxo.SquaredL2Norm(y.size).argshift(-y)
        F = loss * downsample
-       F.diff_lipschitz = F.estimate_diff_lipschitz()
 
-       cv = CV(f=F, g=0.01 * pxo.L1Norm(N), h=0.1 * pxo.L1Norm(N), K=D)
+       cv = CV(f=F, g=0.01 * pxo.L1Norm(N), h=0.1 * pxo.L1Norm((N)), K=D)
        x0, z0 = np.zeros((2, N))
        cv.fit(x0=x0, z0=z0)
        x_recons = cv.solution()
 
        plt.figure()
        plt.stem(x, linefmt="C0-", markerfmt="C0o")
-       mask_ids = np.where(downsample.downsampling_mask)[0]
+       mask_ids = np.where(downsample.adjoint(np.ones_like(y)))[0]
        markerline, stemlines, baseline = plt.stem(mask_ids, y, linefmt="C3-", markerfmt="C3o")
        markerline.set_markerfacecolor("none")
        plt.stem(x_recons, linefmt="C1--", markerfmt="C1s")
@@ -709,29 +707,27 @@ class PD3O(_PrimalDualSplitting):
        import matplotlib.pyplot as plt
        import numpy as np
        import pyxu.operator as pxo
-       from pyxu.experimental._dev import DownSampling, FirstDerivative
+       from pyxu.operator import SubSample, PartialDerivative
        from pyxu.opt.solver import PD3O
 
        x = np.repeat(np.asarray([0, 2, 1, 3, 0, 2, 0]), 10)
        N = x.size
 
-       D = FirstDerivative(size=N, kind="forward")
-       D.lipschitz = D.estimate_lipschitz()
+       D = PartialDerivative.finite_difference(dim_shape=(N,), order=(1,))
 
-       downsample = DownSampling(size=N, downsampling_factor=3)
+       downsample = SubSample(N, slice(None, None, 3))
        y = downsample(x)
        loss = (1 / 2) * pxo.SquaredL2Norm(y.size).argshift(-y)
        F = loss * downsample
-       F.diff_lipschitz = F.estimate_diff_lipschitz()
 
-       pd3o = PD3O(f=F, g=0.01 * pxo.L1Norm(N), h=0.1 * pxo.L1Norm(N), K=D)
+       pd3o = PD3O(f=F, g=0.01 * pxo.L1Norm(N), h=0.1 * pxo.L1Norm((N)), K=D)
        x0, z0 = np.zeros((2, N))
        pd3o.fit(x0=x0, z0=z0)
        x_recons = pd3o.solution()
 
        plt.figure()
        plt.stem(x, linefmt="C0-", markerfmt="C0o")
-       mask_ids = np.where(downsample.downsampling_mask)[0]
+       mask_ids = np.where(downsample.adjoint(np.ones_like(y)))[0]
        markerline, stemlines, baseline = plt.stem(mask_ids, y, linefmt="C3-", markerfmt="C3o")
        markerline.set_markerfacecolor("none")
        plt.stem(x_recons, linefmt="C1--", markerfmt="C1s")
