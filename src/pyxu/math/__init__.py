fe210b020dbf22174334671b13e5ed75b1ea7897
Sepand KASHANI <sepand@kashani.ch>
2025-03-12 11:19:15 +0100

    [math] Add hadamard_outer().

--- src/pyxu/math/__init__.py
+++ src/pyxu/math/__init__.py
@@ -1,4 +1,5 @@
 from .linalg import (
+    hadamard_outer as hadamard_outer,
     pinv as pinv,
     svdvals as svdvals,
 )
--- src/pyxu/math/linalg.py
+++ src/pyxu/math/linalg.py
@@ -7,6 +7,7 @@ import jax.numpy as jnp
 import jax.random as jnd
 import jaxtyping as jt
 import lineax as lx
+import opt_einsum as oe
 import optax
 import optax.tree_utils as otu
 
@@ -129,3 +130,51 @@ def pinv(op: LinearOperator, y: Arrays, tau: jt.Scalar = 0, **kwargs) -> Arrays:
 
     x = solution.value
     return x, solution
+
+
+def hadamard_outer(x: Array, *args: list[Array]) -> Array:
+    r"""
+    Compute Hadamard product of `x` with outer product of `args`:
+
+    .. math::
+
+       y = x \odot (A_{1} \otimes\cdots\otimes A_{D})
+
+    Parameters
+    ----------
+    x: Array
+        (N1,...,ND)
+    args[k]: Array
+        (Nk,)
+
+    Returns
+    -------
+    y: Array
+        (N1,...,ND)
+
+    Note
+    ----
+    All inputs must share the same dtype precision.
+    """
+    D = len(args)
+    assert all(A.ndim == 1 for A in args)
+    sh = tuple(A.size for A in args)
+
+    assert x.shape == sh
+
+    x_ind = (*range(D),)
+    o_ind = (*range(D),)
+    outer_args = [None] * (2 * D)
+    for d in range(D):
+        outer_args[2 * d] = args[d]
+        outer_args[2 * d + 1] = (d,)
+
+    y = oe.contract(  # (N1,...,ND)
+        *(x, x_ind),
+        *outer_args,
+        o_ind,
+        use_blas=True,
+        optimize="auto",
+        backend="jax",
+    )
+    return y
